8d777d4008a5571690a15ed09735648c061bb33f
lcolladotor <lcollado@jhsph.edu>
2021-09-01 16:49:03 -0400

    Adapt code to run a test based on Stephen William's input. Use the latest samtools (v1.13) for this

--- code/bamtofastq/spaceranger_our_alignments_test.sh
+++ code/bamtofastq/spaceranger_our_alignments_test.sh
@@ -2,11 +2,11 @@
 #$ -cwd
 #$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=200G
 #$ -pe local 4
-#$ -N spaceranger_our_alignments
-#$ -o logs/spaceranger_our_alignments.$TASK_ID.txt
-#$ -e logs/spaceranger_our_alignments.$TASK_ID.txt
+#$ -N spaceranger_our_alignments_test
+#$ -o logs/spaceranger_our_alignments_test.$TASK_ID.txt
+#$ -e logs/spaceranger_our_alignments_test.$TASK_ID.txt
 #$ -m e
-#$ -t 1-10
+#$ -t 1-2
 #$ -tc 5
 
 echo "**** Job starts ****"
@@ -21,6 +21,7 @@ echo "Task id: ${SGE_TASK_ID}"
 
 ## load bamtofastq
 module load bamtofastq/1.3.2
+module load samtools/1.13
 
 ## List current modules for reproducibility
 module list
@@ -31,15 +32,19 @@ echo "Processing sample ${SAMPLE}"
 date
 
 ## List current input file
-INPUTBAM="../../raw-data/10x_files/Lieber_Transfer/${SAMPLE}/possorted_genome_bam.bam"
+INITIALBAM="../../raw-data/10x_files/Lieber_Transfer/${SAMPLE}/possorted_genome_bam.bam"
+INPUTBAM="../../raw-data/10x_files/Lieber_Transfer/${SAMPLE}/possorted_genome_bam_newheader.bam"
+
+## Add missing SAM headers Stephen Williams mentioned via email
+(samtools view -H ${INITIALBAM}; echo -e "@CO\t10x_bam_to_fastq:R1(CR:CY,UR:UY)"; echo -e "@CO\t10x_bam_to_fastq:R2(SEQ:QUAL)") | samtools reheader - ${INITIALBAM} > ${INPUTBAM}
 ls -lh ${INPUTBAM}
 
 ## Create output directory
-mkdir -p ../../raw-data/FASTQ/spaceranger_our_alignments
-OUTPUTDIR="../../raw-data/FASTQ/spaceranger_our_alignments/${SAMPLE}/"
+mkdir -p ../../raw-data/FASTQ/spaceranger_our_alignments_test
+OUTPUTDIR="../../raw-data/FASTQ/spaceranger_our_alignments_test/${SAMPLE}/"
 
 ## Run bamtofastq
-bamtofastq --nthreads=4 --traceback --cr11 ${INPUTBAM} ${OUTPUTDIR}
+bamtofastq --nthreads=4 --traceback ${INPUTBAM} ${OUTPUTDIR}
 
 echo "**** Job ends ****"
 date
