9a9db1916342bc791a0a32b783e3d2ceedafb83e
lcolladotor <lcollado@jhsph.edu>
2021-10-11 10:02:27 -0400

    Adapt from #1 in preparation for #17

--- code/01_bamtofastq/spaceranger_their_alignments_nocr11_targeted.sh
+++ code/01_bamtofastq/spaceranger_their_alignments_nocr11_targeted.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -l bluejay,mem_free=5G,h_vmem=5G,h_fsize=200G
 #$ -pe local 4
-#$ -N spaceranger_their_alignments_nocr11
-#$ -o logs/spaceranger_their_alignments_nocr11.$TASK_ID.txt
-#$ -e logs/spaceranger_their_alignments_nocr11.$TASK_ID.txt
+#$ -N spaceranger_their_alignments_nocr11_targeted
+#$ -o logs/spaceranger_their_alignments_nocr11_targeted.$TASK_ID.txt
+#$ -e logs/spaceranger_their_alignments_nocr11_targeted.$TASK_ID.txt
 #$ -m e
 #$ -t 1-10
 #$ -tc 10
@@ -27,13 +27,13 @@ module load samtools/1.13
 module list
 
 ## Locate file
-SAMPLE=$(awk "NR==${SGE_TASK_ID}" samples.txt)
+SAMPLE=$(awk "NR==${SGE_TASK_ID}" samples_targeted_sequencing.txt)
 echo "Processing sample ${SAMPLE}"
 date
 
 ## List current input file
-INITIALBAM="../../raw-data/10x_files/Lieber_Transfer_10x_Alignments/${SAMPLE}/possorted_genome_bam.bam"
-INPUTBAM="../../raw-data/10x_files/Lieber_Transfer_10x_Alignments/${SAMPLE}/possorted_genome_bam_newheader.bam"
+INITIALBAM="../../raw-data/targeted_sequencing/Visium_Neuro_Panel/${SAMPLE}/possorted_genome_bam.bam"
+INPUTBAM="../../raw-data/targeted_sequencing/Visium_Neuro_Panel/${SAMPLE}/possorted_genome_bam_newheader.bam"
 
 ## Add missing SAM headers Stephen Williams mentioned via email
 (samtools view -H ${INITIALBAM}; echo -e "@CO\t10x_bam_to_fastq:R1(CR:CY,UR:UY)"; echo -e "@CO\t10x_bam_to_fastq:R2(SEQ:QUAL)") | samtools reheader - ${INITIALBAM} > ${INPUTBAM}
