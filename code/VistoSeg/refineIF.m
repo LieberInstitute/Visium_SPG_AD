2d9c1c0a29b8c993f154f103ff187d3d0e411c81
madhavitippani <madhavitippani28@gmail.com>
2021-10-19 14:21:20 -0400

    new functions for segmentation with size thresholds and code to remove hallow regions

--- code/VistoSeg/refineIF.m
+++ code/VistoSeg/refineIF.m
@@ -1,108 +1,68 @@
-function refineIF(filename,I,A,M,G,p)
+function refineIF(filename,I,a,g,m,p)
 img = load(filename);
-DAPI = rescale(img.DAPI);
-Abeta = rescale(img.Abeta);
-MAP2 = rescale(img.MAP2);
-GFAP = rescale(img.GFAP);
-LIP = rescale(img.Lipofuscin);
-pTau = rescale(img.pTau);
+O = fieldnames(img);
 
-TDAPI = graythresh(DAPI);
-TAbeta = graythresh(Abeta); if ~isempty(A), TAbeta = A; end
-TMAP2 = graythresh(MAP2); if ~isempty(M), TMAP2 = M; end
-TGFAP = graythresh(GFAP); if ~isempty(G), TGFAP = G; end
-TLIP = graythresh(LIP);
-TpTau = graythresh(pTau); if ~isempty(p), TpTau = p; end
+for i = 1:numel(O)
+    Re.(O{i}) = rescale(img.(O{i}));
+    T.(O{i}) = graythresh(Re.(O{i})); 
+end
+clear img
 
 figure('Visible','off')
-subplot(2,3,1)
-imhist(Abeta(:));
+for i = 1:numel(O)
+subplot(2,3,i)
+imhist(Re.(O{i})(:));
 ylim([0 2.5*10^7])
-hold on
-xline(TAbeta,'r')
-if ~isempty(A)
-xline(A,'g',{num2str(A)})
+xline(T.(O{i}),'r',{num2str(T.(O{i}))});
+
+if contains('Abeta',O{i}) && ~isempty(a)
+    T.Abeta = a; 
+    xline(T.(O{i}),'g',{num2str(T.(O{i}))});
+elseif contains('GFAP',O{i}) && ~isempty(g)
+    T.GFAP = g; 
+    xline(T.(O{i}),'g',{num2str(T.(O{i}))});
+elseif contains('MAP2',O{i}) && ~isempty(m)
+    T.MAP2 = m; 
+    xline(T.(O{i}),'g',{num2str(T.(O{i}))});
+elseif contains('pTau',O{i}) && ~isempty(p)
+    T.pTau = p; 
+    xline(T.(O{i}),'g',{num2str(T.(O{i}))});
 end
-title(['Abeta ',num2str(TAbeta),''])
+title(O{i})
 
-subplot(2,3,2)
-imhist(MAP2(:));
-ylim([0 2.5*10^7])
-xline(TMAP2,'r')
-if ~isempty(M)
-xline(M,'g',{num2str(M)})
+B.(O{i}) = imbinarize(Re.(O{i}),T.(O{i})); 
 end
-title(['MAP2 ',num2str(TMAP2),''])
-
-subplot(2,3,3)
-imhist(GFAP(:));
-ylim([0 2.5*10^7])
-xline(TGFAP,'r')
-if ~isempty(G)
-xline(G,'g',{num2str(G)})
-end
-title(['GFAP ',num2str(TGFAP),''])
-
-subplot(2,3,4)
-imhist(LIP(:));
-ylim([0 2.5*10^7])
-xline(TLIP,'r')
-title(['Lip ',num2str(TLIP),''])
-
-subplot(2,3,5)
-imhist(DAPI(:));
-ylim([0 2.5*10^7])
-xline(TDAPI,'r')
-title(['DAPI ',num2str(TDAPI)])
-
-subplot(2,3,6)
-imhist(pTau(:));
-ylim([0 2.5*10^7])
-xline(TpTau,'r')
-if ~isempty(p)
-xline(p,'g',{num2str(p)})
-end
-title(['pTau ',num2str(TpTau),''])
-
 saveas(gcf,[filename(1:end-4),'_thresh.png'])
 close all
 
-BDAPI = imbinarize(DAPI,TDAPI);
-BLIP = imbinarize(LIP,TLIP);
-BAbeta = imbinarize(Abeta,TAbeta);
-BMAP2 = imbinarize(MAP2,TMAP2);
-BGFAP = imbinarize(GFAP,TGFAP);
-BpTau = imbinarize(pTau,TpTau);
+M = B;
+for i = 1:numel(O)
+M.(O{i})(B.Lipofuscin|B.MAP2|B.GFAP) = 0;
+end
 
-O = fieldnames(img);
-IMG = [Abeta(I(1):I(2),I(3):I(4)),ones(1000,20),MAP2(I(1):I(2),I(3):I(4)),ones(1000,20),mat2gray(GFAP(I(1):I(2),I(3):I(4))),ones(1000,20),LIP(I(1):I(2),I(3):I(4)),ones(1000,20),DAPI(I(1):I(2),I(3):I(4)),ones(1000,20),pTau(I(1):I(2),I(3):I(4))];
-IMG1 = [BAbeta(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
 
-BAbeta(BLIP|BMAP2|BGFAP) = 0;
-BpTau(BLIP|BMAP2|BGFAP) = 0;
-BGFAP(BLIP|BMAP2|BGFAP) = 0;
-BMAP2(BLIP|BMAP2|BGFAP) = 0;
-BLIP(BLIP|BMAP2|BGFAP) = 0;
+IMG = [];
+IMG1 = [];
+IMG2 = [];
+for i = 1:numel(O)
+IMG = [IMG,Re.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+IMG1 = [IMG1,B.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+IMG2 = [IMG2,M.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+end
 
-IMG2 = [BAbeta(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
-
-BW = bwareaopen(BAbeta, 50);
-
-IMG3 = [BW(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
+BW = bwareaopen(M.Abeta, 1500);
+%M.Abeta = BW;
+M.Abeta = bwpropfilt(BW, 'Solidity', [0.5, 1]); 
+M.DAPI = B.DAPI;
+IMG3 = [];
+for i = 1:numel(O)
+IMG3 = [IMG3,M.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+end
 
 imwrite([IMG;ones(20,size(IMG,2));IMG1;ones(20,size(IMG,2));IMG2;ones(20,size(IMG,2));IMG3],[filename(1:end-4),'_test.png'])
+imwrite(M.Abeta,[filename(1:end-4),'_Abeta.png'])
+imwrite(M.pTau,[filename(1:end-4),'_pTau.png'])
 
-imwrite(BW,[filename(1:end-4),'_Abeta.png'])
-imwrite(BpTau,[filename(1:end-4),'_pTau.png'])
-
-Segmentations.Abeta = BW;
-Segmentations.MAP2 = BMAP2;
-Segmentations.GFAP = BGFAP;
-Segmentations.Lipofuscin = BLIP;
-Segmentations.DAPI = BDAPI;
-Segmentations.pTau = BpTau;
-
-save([filename(1:end-4),'_segmentation.mat'], '-struct', 'Segmentations')
-
+save([filename(1:end-4),'_segmentation.mat'], '-struct', 'M')
 
 end
--- code/VistoSeg/segmentIF.m
+++ code/VistoSeg/segmentIF.m
@@ -1,115 +1,49 @@
 function segmentIF(filename,I)
 
 img = load(filename);
-DAPI = rescale(img.DAPI);
-Abeta = rescale(img.Abeta);
-MAP2 = rescale(img.MAP2);
-GFAP = rescale(img.GFAP);
-LIP = rescale(img.Lipofuscin);
-pTau = rescale(img.pTau);
+O = fieldnames(img);
+
+for i = 1:numel(O)
+    Re.(O{i}) = rescale(img.(O{i}));
+    T.(O{i}) = graythresh(Re.(O{i}));  
+    B.(O{i}) = imbinarize(Re.(O{i}),T.(O{i})); 
+end
+
+M = B;
 
 figure('Visible','off')
-[counts,x] = imhist(Abeta(:));
-stem(x,counts,'g')
-hold on
-[counts,x] = imhist(MAP2(:));
-stem(x,counts,'m')
-[counts,x] = imhist(GFAP(:));
-stem(x,counts,'r')
-[counts,x] = imhist(LIP(:));
-stem(x,counts,'c')
-[counts,x] = imhist(DAPI(:));
-stem(x,counts,'b')
-[counts,x] = imhist(pTau(:));
-stem(x,counts,'y')
+for i = 1:numel(O)
+subplot(2,3,i)
+imhist(Re.(O{i})(:));
 ylim([0 2.5*10^7])
-
-saveas(gcf,[filename(1:end-4),'_hists.png'])
-close all
-
-TDAPI = graythresh(DAPI);
-TAbeta = graythresh(Abeta);
-TMAP2 = graythresh(MAP2);
-TGFAP = graythresh(GFAP);
-TLIP = graythresh(LIP);
-TpTau = graythresh(pTau);
-
-figure('Visible','off')
-subplot(2,3,1)
-imhist(Abeta(:));
-ylim([0 2.5*10^7])
-xline(TAbeta,'r')
-title(['Abeta ',num2str(TAbeta),''])
-
-subplot(2,3,2)
-imhist(MAP2(:));
-ylim([0 2.5*10^7])
-xline(TMAP2,'r')
-title(['MAP2 ',num2str(TMAP2),''])
-
-subplot(2,3,3)
-imhist(GFAP(:));
-ylim([0 2.5*10^7])
-xline(TGFAP,'r')
-title(['GFAP ',num2str(TGFAP),''])
-
-subplot(2,3,4)
-imhist(LIP(:));
-ylim([0 2.5*10^7])
-xline(TLIP,'r')
-title(['Lip ',num2str(TLIP),''])
-
-subplot(2,3,5)
-imhist(DAPI(:));
-ylim([0 2.5*10^7])
-xline(TDAPI,'r')
-title(['DAPI ',num2str(TDAPI)])
-
-subplot(2,3,6)
-imhist(pTau(:));
-ylim([0 2.5*10^7])
-xline(TpTau,'r')
-title(['pTau ',num2str(TpTau),''])
-
+xline(T.(O{i}),'r',{num2str(T.(O{i}))});
+title(O{i})
+M.(O{i})(B.Lipofuscin|B.MAP2|B.GFAP) = 0;
+end
 saveas(gcf,[filename(1:end-4),'_thresh.png'])
 close all
 
-BDAPI = imbinarize(DAPI,TDAPI);
-BLIP = imbinarize(LIP,TLIP);
+IMG = [];
+IMG1 = [];
+IMG2 = [];
+for i = 1:numel(O)
+IMG = [IMG,Re.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+IMG1 = [IMG1,B.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+IMG2 = [IMG2,M.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+end
 
-BAbeta = imbinarize(Abeta,TAbeta);
-BMAP2 = imbinarize(MAP2,TMAP2);
-BGFAP = imbinarize(GFAP,TGFAP);
-BpTau = imbinarize(pTau,TpTau);
-
-O = fieldnames(img);
-IMG = [Abeta(I(1):I(2),I(3):I(4)),ones(1000,20),MAP2(I(1):I(2),I(3):I(4)),ones(1000,20),mat2gray(GFAP(I(1):I(2),I(3):I(4))),ones(1000,20),LIP(I(1):I(2),I(3):I(4)),ones(1000,20),DAPI(I(1):I(2),I(3):I(4)),ones(1000,20),pTau(I(1):I(2),I(3):I(4))];
-IMG1 = [BAbeta(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
-
-BAbeta(BLIP|BMAP2|BGFAP) = 0;
-BpTau(BLIP|BMAP2|BGFAP) = 0;
-BGFAP(BLIP|BMAP2|BGFAP) = 0;
-BMAP2(BLIP|BMAP2|BGFAP) = 0;
-BLIP(BLIP|BMAP2|BGFAP) = 0;
-
-IMG2 = [BAbeta(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
-
-BW = bwareaopen(BAbeta, 50);
-
-IMG3 = [BW(I(1):I(2),I(3):I(4)),ones(1000,20),BMAP2(I(1):I(2),I(3):I(4)),ones(1000,20),BGFAP(I(1):I(2),I(3):I(4)),ones(1000,20),BLIP(I(1):I(2),I(3):I(4)),ones(1000,20),BDAPI(I(1):I(2),I(3):I(4)),ones(1000,20),BpTau(I(1):I(2),I(3):I(4))];
+BW = bwareaopen(M.Abeta, 200);
+M.Abeta = BW;
+M.DAPI = B.DAPI;
+IMG3 = [];
+for i = 1:numel(O)
+IMG3 = [IMG3,M.(O{i})(I(1):I(2),I(3):I(4)),ones(1000,20)];
+end
 
 imwrite([IMG;ones(20,size(IMG,2));IMG1;ones(20,size(IMG,2));IMG2;ones(20,size(IMG,2));IMG3],[filename(1:end-4),'_test.png'])
-
 imwrite(BW,[filename(1:end-4),'_Abeta.png'])
-imwrite(BpTau,[filename(1:end-4),'_pTau.png'])
+imwrite(M.pTau,[filename(1:end-4),'_pTau.png'])
 
-Segmentations.Abeta = BW;
-Segmentations.MAP2 = BMAP2;
-Segmentations.GFAP = BGFAP;
-Segmentations.Lipofuscin = BLIP;
-Segmentations.DAPI = BDAPI;
-Segmentations.pTau = BpTau;
-
-save([filename(1:end-4),'_segmentation.mat'], '-struct', 'Segmentations')		 
+save([filename(1:end-4),'_segmentation.mat'], '-struct', 'M')		 
 
 end
