c374867e662f05098c3bdd975f524c64148b5ada
madhavitippani <madhavitippani28@gmail.com>
2021-10-21 11:36:05 -0400

    shell script to batch run countSpots

--- code/VistoSeg/countSpots.sh
+++ code/VistoSeg/countSpots.sh
@@ -2,12 +2,12 @@
 #$ -cwd
 #$ -pe local 5
 #$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
-#$ -o /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/logs/countSpots_8.txt
-#$ -e /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/logs/countSpots_8.txt
+#$ -o /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/logs/countSpots.$TASK_ID.txt
+#$ -e /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/logs/countSpots.$TASK_ID.txt
 #$ -m e
 #$ -M madhavitippani28@gmail.com
-#$ -t 12
-#$ -tc 3
+#$ -t 1
+#$ -tc 5
 
  
 echo "**** Job starts ****"
@@ -20,13 +20,17 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
+echo "Sample id: $(cat /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/listOFfiles.txt | awk "NR==${SGE_TASK_ID}")"
+echo "****"
+
+## List current modules for reproducibility
+module list
+
+mask=$(awk 'BEGIN {FS="\t"} {print $1}' /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/listOFfiles.txt | awk "NR==${SGE_TASK_ID}")
+jsonname=$(awk 'BEGIN {FS="\t"} {print $2}' /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/listOFfiles.txt | awk "NR==${SGE_TASK_ID}")
+posname=$(awk 'BEGIN {FS="\t"} {print $3}' /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/listOFfiles.txt | awk "NR==${SGE_TASK_ID}")
 
 toolbox='/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg/'
-
-mask='/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Capture_Areas/VIFAD2_V10A27-106_D1_segmentation.mat'
-jsonname='/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/spaceranger/V10A27106_D1_Br3880/outs/spatial/scalefactors_json.json'
-posname='/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/spaceranger/V10A27106_D1_Br3880/outs/spatial/tissue_positions_list.csv'
-
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$mask','$jsonname','$posname')" 
 
 echo "**** Job ends ****"
