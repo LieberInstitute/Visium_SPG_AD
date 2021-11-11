13994426bcb858db1d3d17642b0c9449ad105ed5
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-11-11 01:16:33 -0500

    Simplify app.R code

--- code/05_deploy_app/app.R
+++ code/05_deploy_app/app.R
@@ -1,5 +1,4 @@
 library("spatialLIBD")
-library("here")
 library("markdown") ## Hm... to avoid this error
 # 2021-11-11T05:30:49.941401+00:00 shinyapps[5096402]: Listening on http://127.0.0.1:32863
 # 2021-11-11T05:30:50.218127+00:00 shinyapps[5096402]: Warning: Error in loadNamespace: there is no package called ‘markdown’
@@ -14,13 +13,9 @@ options(repos = BiocManager::repositories())
 ## Load the data
 # spe <- readRDS("spe_workflow_Visium_spatialLIBD.rds")
 
+## Create a soft link to the data, otherwise rsconnect::deployApp doesn't work
 # system("ln -s ../../processed-data/spe/spe_raw.Rdata spe_raw.Rdata")
-local_path <- here::here("processed-data", "spe", "spe_raw.Rdata")
-if(file.exists(local_path)){
-    load(local_path, verbose = TRUE)
-} else {
-    load("spe_raw.Rdata", verbose = TRUE)
-}
+load("spe_raw.Rdata", verbose = TRUE)
 spe_raw <- spe_raw[, spatialData(spe_raw)$in_tissue]
 # rm(spe_raw)
 
