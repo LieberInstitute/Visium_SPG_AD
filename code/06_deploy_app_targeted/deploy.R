ec5c2ffb6486276899bbbb70ebf09d9218148a05
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-12-01 15:43:03 -0500

    Copied the 05_deploy_app code into 06_deploy_app_targeted
    Co-authored-by: Sang Ho Kwon <skwon17@jh.edu>

---
+++ code/06_deploy_app_targeted/deploy.R
@@ -0,0 +1,26 @@
+library("rsconnect")
+library("here")
+
+## Or you can go to your shinyapps.io account and copy this
+## Here we do this to keep our information hidden.
+load(here("code", "05_deploy_app", ".deploy_info.Rdata"), verbose = TRUE)
+rsconnect::setAccountInfo(
+    name = deploy_info$name,
+    token = deploy_info$token,
+    secret = deploy_info$secret
+)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Deploy the app, that is, upload it to shinyapps.io
+rsconnect::deployApp(
+    appDir = here("code", "05_deploy_app"),
+    appFiles = c(
+        "app.R",
+        "spe.Rdata"
+    ),
+    appName = 'Visium_IF_AD_Kwon2021',
+    account = 'libd',
+    server = 'shinyapps.io'
+)
