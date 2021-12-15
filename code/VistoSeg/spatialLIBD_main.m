b84d077ab5e350166df516ebf8c0f3422adfebe5
lcolladotor <lcollado@jhsph.edu>
2021-12-15 11:30:07 -0500

    Fully resolve #18
    All images are ready now =) Verified with the code below:
    
    ```R
    getwd()
    img <- dir()
    sample <- gsub("_p.*|_DAPI.*|_m.*|_Ab.*", "", img)
    table(sample)
    stopifnot(all(table(sample) == 16))
    ```
    
    ```R
    > getwd()
    [1] "/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/spatialLIBD_images"
    > img <- dir()
    > sample <- gsub("_p.*|_DAPI.*|_m.*|_Ab.*", "", img)
    > table(sample)
    sample
    V10A27004_A1_Br3874 V10A27004_D1_Br3880 V10A27106_A1_Br3874 V10A27106_B1_Br3854
                     16                  16                  16                  16
    V10A27106_C1_Br3873 V10A27106_D1_Br3880 V10T31036_A1_Br3874 V10T31036_B1_Br3854
                     16                  16                  16                  16
    V10T31036_C1_Br3873 V10T31036_D1_Br3880
                     16                  16
    > stopifnot(all(table(sample) == 16))
    ```
    
    Co-authored-by: Madhavi Tippani <madhavitippani28@gmail.com>

--- code/VistoSeg/spatialLIBD_main.m
+++ code/VistoSeg/spatialLIBD_main.m
@@ -1,5 +1,23 @@
 cd /dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/VistoSeg
 
+filename = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Segmentations/VIFAD2_V10A27-106_A1_segmentation.mat';
+filename1 = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Capture_Areas/VIFAD2_V10A27-106_A1.mat';
+slide = 'V10A27106';
+array = 'A1';
+brain = 'Br3874';
+spatialLIBD(filename,filename1,slide,array,brain)
+disp(['finished ',slide,' ',array])
+clear all
+
+filename = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Segmentations/VIFAD2_V10A27-106_B1_segmentation.mat';
+filename1 = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Capture_Areas/VIFAD2_V10A27-106_B1.mat';
+slide = 'V10A27106';
+array = 'B1';
+brain = 'Br3854';
+spatialLIBD(filename,filename1,slide,array,brain)
+disp(['finished ',slide,' ',array])
+clear all
+
 filename = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Segmentations/VIFAD2_V10A27-106_C1_segmentation.mat';
 filename1 = '/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/processed-data/Images/VistoSeg/Capture_Areas/VIFAD2_V10A27-106_C1.mat';
 slide = 'V10A27106';
