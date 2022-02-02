7bef4ea1962624132cc494769d110bdc8f291b27
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-02-01 20:09:34 -0500

    Prepare code for running under qsub

--- code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
+++ code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
@@ -2,12 +2,15 @@
 library("getopt")
 library("here")
 library("SpatialExperiment")
+library("spatialLIBD")
 library("sessioninfo")
 library("scran") ## requires uwot for UMAP
 library("uwot")
 library("scater")
 library("BiocParallel")
 library("PCAtools")
+library("ggplot2")
+library("BayesSpace")
 
 ## Specify parameters
 spec <- matrix(c(
@@ -28,6 +31,7 @@ dir_plots <- here::here("plots", "08_harmony_BayesSpace")
 dir_rdata <- here::here("processed-data", "08_harmony_BayesSpace")
 dir.create(dir_plots, showWarnings = FALSE, recursive = TRUE)
 dir.create(dir_rdata, showWarnings = FALSE, recursive = TRUE)
+dir.create(file.path(dir_rdata, "clustering_results"), showWarnings = FALSE)
 
 ## Load the data
 load(here::here("processed-data", "07_spot_qc", opt$spefile), verbose = TRUE)
@@ -38,7 +42,7 @@ if (opt$spefile == "spe_targeted_postqc.Rdata") {
     spe <- spe_targeted
 }
 
-
+message("Running quickCluster()")
 set.seed(20220201)
 Sys.time()
 spe$scran_quick_cluster <- quickCluster(
@@ -49,8 +53,8 @@ spe$scran_quick_cluster <- quickCluster(
 )
 Sys.time()
 
+message("Running computeSumFactors()")
 Sys.time()
-# [1] "2021-12-15 13:19:48 EST"
 ## Might be needed:
 # options(error = recover)
 spe <-
@@ -62,13 +66,13 @@ Sys.time()
 
 table(spe$scran_quick_cluster)
 
+message("Running checking sizeFactors()")
 summary(sizeFactors(spe))
-# Min.   1st Qu.    Median      Mean   3rd Qu.      Max.
-# 0.000034  0.477444  0.847973  1.000000  1.331274 16.813313
 
 spe <- logNormCounts(spe)
 
 
+message("Running modelGeneVar()")
 ## From
 ## http://bioconductor.org/packages/release/bioc/vignettes/scran/inst/doc/scran.html#4_variance_modelling
 dec <- modelGeneVar(spe,
@@ -95,16 +99,15 @@ mapply(function(block, blockname) {
 }, dec$per.block, names(dec$per.block))
 dev.off()
 
+message("Running getTopHVGs()")
 top.hvgs <- getTopHVGs(dec, prop = 0.1)
 length(top.hvgs)
 
 top.hvgs.fdr5 <- getTopHVGs(dec, fdr.threshold = 0.05)
 length(top.hvgs.fdr5)
-# [1] 18417
 
 top.hvgs.fdr1 <- getTopHVGs(dec, fdr.threshold = 0.01)
 length(top.hvgs.fdr1)
-# [1] 17830
 
 save(top.hvgs,
     top.hvgs.fdr5,
@@ -113,7 +116,7 @@ save(top.hvgs,
 )
 
 
-
+message("Running runPCA()")
 set.seed(20220201)
 Sys.time()
 spe <- runPCA(spe, subset_row = top.hvgs, ncomponents = 50)
@@ -123,7 +126,6 @@ Sys.time()
 percent.var <- attr(reducedDim(spe, "PCA"), "percentVar")
 chosen.elbow <- PCAtools::findElbowPoint(percent.var)
 chosen.elbow
-# 50
 
 pdf(
     file.path(dir_plots, "pca_elbow_final.pdf"),
@@ -133,31 +135,18 @@ plot(percent.var, xlab = "PC", ylab = "Variance explained (%)")
 abline(v = chosen.elbow, col = "red")
 dev.off()
 
+message("Running runTSNE() perplexity 5")
 Sys.time()
 set.seed(20220201)
 spe <-
     runTSNE(spe,
         dimred = "PCA",
-        name = "TSNE_perplexity50",
-        perplexity = 50
-    )
-Sys.time()
-# [1] "2021-02-17 10:45:30 EST"
-# [1] "2021-02-17 11:02:59 EST"
-
-
-Sys.time()
-set.seed(20220201)
-spe <-
-    runTSNE(spe,
-        dimred = "PCA",
-        name = "TSNE_perplexity5",
+        name = "TSNE_perplexity05",
         perplexity = 5
     )
 Sys.time()
-# [1] "2021-09-08 11:14:41 EDT"
-# [1] "2021-09-08 12:03:04 EDT"
 
+message("Running runTSNE() perplexity 20")
 Sys.time()
 set.seed(20220201)
 spe <-
@@ -167,9 +156,19 @@ spe <-
         perplexity = 20
     )
 Sys.time()
-# [1] "2021-09-08 12:17:38 EDT"
-# [1] "2021-09-08 13:09:17 EDT"
 
+message("Running runTSNE() perplexity 50")
+Sys.time()
+set.seed(20220201)
+spe <-
+    runTSNE(spe,
+        dimred = "PCA",
+        name = "TSNE_perplexity50",
+        perplexity = 50
+    )
+Sys.time()
+
+message("Running runTSNE() perplexity 80")
 Sys.time()
 set.seed(20220201)
 spe <-
@@ -181,19 +180,26 @@ spe <-
 Sys.time()
 
 
-
-
-# RunUMAP
+message("Running runUMAP()")
 Sys.time()
 spe <- runUMAP(spe, dimred = "PCA")
 colnames(reducedDim(spe, "UMAP")) <- c("UMAP1", "UMAP2")
 Sys.time()
 
+## Perform harmony batch correction
+message("Running RunHarmony()")
+Sys.time()
+spe <- RunHarmony(spe, "sample_id", verbose = FALSE)
+Sys.time()
+
+message("Running runUMAP() on HARMONY dimensions")
+Sys.time()
+spe <- runUMAP(spe, dimred = "HARMONY", name = "UMAP.HARMONY")
+Sys.time()
+colnames(reducedDim(spe, "UMAP.HARMONY")) <- c("UMAP1", "UMAP2")
 
 
-
-
-# make plots ofUMAP
+## Explore UMAP results
 pdf(file = file.path(dir_plots, "UMAP_subject.pdf"))
 ggplot(
     data.frame(reducedDim(spe, "UMAP")),
@@ -214,12 +220,7 @@ ggplot(
     theme_bw()
 dev.off()
 
-### harmony batch correction
-spe <- RunHarmony(spe, "sample_id", verbose = F)
-spe <- runUMAP(spe, dimred = "HARMONY", name = "UMAP.HARMONY")
-colnames(reducedDim(spe, "UMAP.HARMONY")) <- c("UMAP1", "UMAP2")
-
-
+## Explore UMAP on HARMONY reduced dimensions
 pdf(file = file.path(dir_plots, "UMAP_harmony_sample_id.pdf"))
 ggplot(
     data.frame(reducedDim(spe, "UMAP.HARMONY")),
@@ -231,60 +232,66 @@ ggplot(
 dev.off()
 
 
-## graph-based on batch correct
+## Perform graph-based clustering on batch corrected-data
+message("Running buildSNNGraph() on HARMONY dimensions")
 Sys.time()
 g_k10 <- buildSNNGraph(spe, k = 10, use.dimred = "HARMONY")
 Sys.time()
-# "2021-12-16 16:17:31 EST"
-# "2021-12-16 16:34:28 EST"
 save(g_k10, file = file.path(dir_rdata, "g_k10_harmony.Rdata"))
 
+message("Running cluster_walktrap()")
 Sys.time()
 g_walk_k10 <- igraph::cluster_walktrap(g_k10)
 Sys.time()
-# [1] "2021-12-17 13:05:59 EST"
-
 save(g_walk_k10, file = file.path(dir_rdata, "g_walk_k10_harmony.Rdata"))
 
 clust_k10 <- sort_clusters(g_walk_k10$membership)
-save(clust_k10, file = file.path(dir_rdata, "clust_k10_harmony.Rdata"))
+spe$SNN_k10 <- clust_k10 ## Add this one to the SPE too
+## Export for later use
+cluster_export(
+    spe,
+    "SNN_k10",
+    cluster_dir = file.path(dir_rdata, "clustering_results")
+)
 
+message("Running cut_at() from k = 4 to 28")
 clust_k5_list <- lapply(4:28, function(n) {
     message(paste(Sys.time(), "n =", n))
     sort_clusters(igraph::cut_at(g_walk_k10, n = n))
 })
-names(clust_k5_list) <- paste0("k", 4:28)
-save(clust_k5_list, file = file.path(dir_rdata, "clust_k5_list_harmony.Rdata"))
+names(clust_k5_list) <- paste0("SNN_k10_k", 4:28)
 
 ## Add clusters to spe colData
-cluster_colNames <- paste0("SNN_k10_k", 4:28)
-for (i in seq_along(cluster_colNames)) {
+for (i in seq_along(names(clust_k5_list))) {
     colData(spe) <- cbind(colData(spe), clust_k5_list[i])
+     ## Add proper name
+    colnames(colData(spe))[ncol(colData(spe))] <- names(clust_k5_list)[i]
+
+    ## Export for later use outside the SPE object
+    cluster_export(
+        spe,
+        names(clust_k5_list)[i],
+        cluster_dir = file.path(dir_rdata, "clustering_results")
+    )
+
 }
-colnames(colData(spe))[34:58] <- cluster_colNames
 
 ## make plot
 sample_ids <- unique(colData(spe)$sample_id)
-pdf(file = file.path(dir_plots, "vis_clus_graph_based_har.pdf"))
+pdf(file = file.path(dir_plots, "graph_based_harmony.pdf"))
 for (i in seq_along(sample_ids)) {
-    for (j in seq_along(cluster_colNames)) {
+    for (j in seq_along(names(clust_k5_list))) {
         my_plot <- vis_clus(
             spe = spe,
-            clustervar = cluster_colNames[j],
+            clustervar = names(clust_k5_list)[j],
             sampleid = sample_ids[i],
             colors = mycolors,
-            ... = paste0(" ", cluster_colNames[j])
+            ... = paste0(" ", names(clust_k5_list)[j])
         )
         print(my_plot)
     }
 }
 dev.off()
-
-cluster_export(
-    spe,
-    "SNN_k10_k7",
-    cluster_dir = file.path(dir_rdata, "clustering_results")
-)
 
 ## Save new SPE objects
 if (opt$spefile == "spe_targeted_postqc.Rdata") {
