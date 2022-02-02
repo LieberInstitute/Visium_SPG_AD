c9979c908ec38b6b8f87a1417eb375ea6d6754c5
lcolladotor <lcollado@jhsph.edu>
2022-02-01 20:29:15 -0500

    Add hidden bash scripts

---
+++ code/08_harmony_BayesSpace/.preprocess_and_harmony_spe_postqc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=30G,h_vmem=30G,h_fsize=100G
+#$ -pe local 4
+#$ -N preprocess_and_harmony_spe_postqc
+#$ -o logs/preprocess_and_harmony_spe_postqc.txt
+#$ -e logs/preprocess_and_harmony_spe_postqc.txt
+#$ -m e
+#$ -hold_jid qc_metrics_and_segmentation
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.1.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 01_preprocess_and_harmony.R -s spe_postqc
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
+
