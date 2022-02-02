51950f3f7bd110581f3d1c90f27bdef805c950dd
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-02-01 19:23:06 -0500

    Update code style with code/update_style.R

--- code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
+++ code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
@@ -1,21 +1,21 @@
 ## Required libraries
-library('getopt')
+library("getopt")
 library("here")
 library("SpatialExperiment")
 library("sessioninfo")
 
 ## Specify parameters
 spec <- matrix(c(
-	'spefile', 's', 2, 'character', 'SPE file name',
-	'help' , 'h', 0, 'logical', 'Display help'
-), byrow=TRUE, ncol=5)
+    "spefile", "s", 2, "character", "SPE file name",
+    "help", "h", 0, "logical", "Display help"
+), byrow = TRUE, ncol = 5)
 opt <- getopt(spec)
 
 ## if help was asked for print a friendly message
 ## and exit with a non-zero error code
 if (!is.null(opt$help)) {
-	cat(getopt(spec, usage=TRUE))
-	q(status=1)
+    cat(getopt(spec, usage = TRUE))
+    q(status = 1)
 }
 
 ## Create output directories
@@ -24,20 +24,21 @@ dir_rdata <- here::here("processed-data", "08_harmony_BayesSpace")
 dir.create(dir_plots, showWarnings = FALSE, recursive = TRUE)
 dir.create(dir_rdata, showWarnings = FALSE, recursive = TRUE)
 
-
 ## Load the data
 load(here::here("processed-data", "07_spot_qc", opt$spefile), verbose = TRUE)
 
 ## Rename from spe_targeted to spe to simplify the code so it can work with
 ## either
-if(opt$spefile == "spe_targeted_postqc.Rdata") {
+if (opt$spefile == "spe_targeted_postqc.Rdata") {
     spe <- spe_targeted
 }
 
 
 
+
+
 ## Save new SPE objects
-if(opt$spefile == "spe_targeted_postqc.Rdata") {
+if (opt$spefile == "spe_targeted_postqc.Rdata") {
     spe_targeted <- spe
     ## First time switching the order of the keywords: now targeted is at the
     ## end, which will make it easier to sort the spe files.
