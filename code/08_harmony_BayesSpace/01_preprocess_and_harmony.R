93cbf9db7e77d15bee308da142594dafd57fff15
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-02-01 19:14:56 -0500

    Adapted to have a single "spefile" argument. Adapted the basic code for saving the SPEs from https://github.com/LieberInstitute/Visium_IF_AD/blob/master/code/07_spot_qc/qc_metrics_and_segmentation.R#L336-L338

--- code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
+++ code/08_harmony_BayesSpace/01_preprocess_and_harmony.R
@@ -1,11 +1,9 @@
 ## Required libraries
 library('getopt')
-library('devtools')
 
 ## Specify parameters
 spec <- matrix(c(
-	'outdir', 'o', 2, 'character', 'Path where the output of infer_experiment.py was saved. Defaults to HISAT2_out/infer_strandess',
-    'pattern', 'p', 2, 'character', 'Name of the pattern file. Defaults to inferred_strandness_pattern.txt',
+	'spefile', 's', 2, 'character', 'SPE file name',
 	'help' , 'h', 0, 'logical', 'Display help'
 ), byrow=TRUE, ncol=5)
 opt <- getopt(spec)
@@ -15,4 +13,22 @@ opt <- getopt(spec)
 if (!is.null(opt$help)) {
 	cat(getopt(spec, usage=TRUE))
 	q(status=1)
+}
+
+## Rename from spe_targeted to spe to simplify the code
+if(opt$spefile == "spe_targeted_postqc.Rdata") {
+    spe <- spe_targeted
+}
+
+
+
+## Save new SPE objects
+if(opt$spefile == "spe_targeted_postqc.Rdata") {
+    spe_targeted <- spe
+    ## First time switching the order of the keywords: now targeted is at the
+    ## end, which will make it easier to sort the spe files.
+    save(spe_targeted, file = file.path(dir_rdata, "spe_harmony_targeted.Rdata"))
+} else {
+    ## First time using "wholegenome" in the spe name, to clearly identify it
+    save(spe, file.path(dir_rdata, "spe_harmony_wholegenome.Rdata"))
 }
