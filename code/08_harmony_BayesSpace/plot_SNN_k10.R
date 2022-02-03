17bb8ca7ecd124bfa06c248bad5e4fee29764c7d
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-02-02 19:34:20 -0500

    Fix pdf call and add plot for making grid plots for the SNN_k10_k{4..28} graph-based clustering results

--- code/08_harmony_BayesSpace/plot_SNN_k10.R
+++ code/08_harmony_BayesSpace/plot_SNN_k10.R
@@ -11,22 +11,39 @@ plot_SNN_k10 <- function(suffix) {
         )
     )
 
-    spe <- cluster_import(spe,
-        cluster_dir = here::here("processed-data", "08_harmony_BayesSpace", suffix, "clusters_graphbased")
-    )
-
     cols <- Polychrome::palette36.colors(length(unique(spe$SNN_k10)))
     names(cols) <- unique(spe$SNN_k10)
 
     vis_grid_clus(
         spe = spe,
         clustervar = "SNN_k10",
-        pdf_file = here::here("plots", "08_harmony_BayesSpace", suffix, paste0("graphbased_SNN_k10", k_nice, ".pdf")),
+        pdf_file = here::here("plots", "08_harmony_BayesSpace", suffix, "graphbased_SNN_k10.pdf"),
         sort_clust = FALSE,
         colors = cols,
         spatial = FALSE,
         point_size = 2
     )
+
+    pdf(here::here("plots", "08_harmony_BayesSpace", suffix, "graphbased_SNN_k10_cut_at.pdf"), height = 24, width = 36)
+    for (k in 4:28) {
+        k_nice <- sprintf("%02d", k)
+
+        cols <- Polychrome::palette36.colors(k)
+        k_var <- paste0("SNN_k10_k", k)
+        names(cols) <- unique(colData(spe)[[k_var]])
+
+        p_list <- vis_grid_clus(
+            spe = spe,
+            clustervar = k_var,
+            return_plots = TRUE,
+            sort_clust = FALSE,
+            colors = cols,
+            spatial = FALSE,
+            point_size = 2
+        )
+        print(cowplot::plot_grid(plotlist = p_list))
+    }
+    dev.off()
 
     return(NULL)
 }
