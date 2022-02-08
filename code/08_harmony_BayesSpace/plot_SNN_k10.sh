785591a36a243e937be62d5afffd646cfc9f906c
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-02-08 17:02:07 -0500

    Add code for updating the large PDFs with the graph-based clustering results via qsub

---
+++ code/08_harmony_BayesSpace/plot_SNN_k10.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=80G,h_vmem=80G,h_fsize=100G
+#$ -N plot_SNN_k10
+#$ -o logs/plot_SNN_k10.txt
+#$ -e logs/plot_SNN_k10.txt
+#$ -m e
+##$ -hold_jid preprocess_and_harmony_spe_postqc,preprocess_and_harmony_spe_targeted_postqc
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.1.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 03_plot_SNN_k10.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
