7851af9890468cd419bd31f89c7edc85c3be9a6e
sparthib <sparthi1@jhu.edu>
2022-02-09 06:50:35 -0500

    define thresholds
    reproducibility info

--- code/07_spot_qc/compare_pathology_spots_to_percent.R
+++ code/07_spot_qc/compare_pathology_spots_to_percent.R
@@ -13,7 +13,33 @@ library("sessioninfo")
 load(here::here("processed-data", "07_spot_qc", "spe_postqc.Rdata"), verbose = TRUE)
 load(here::here("processed-data", "07_spot_qc", "spe_targeted_postqc.Rdata"), verbose = TRUE)
 
-## Create output directories
+## output directories
 dir_plots <- here::here("plots", "07_spot_qc")
-dir.create(dir_plots, showWarnings = FALSE)
 
+
+
+n_threshold = 0   #number threshold
+p_threshold = 0.01  #percentage threshold
+
+
+##pathology_df
+
+
+''' path_df_spe <- data.frame(
+    spot_id =rownames(colData(spe)),
+    sample_id = colData(spe)$sample_id,
+    NAbeta = colData(spe)$NAbeta,
+    NpTau = colData(spe)$NpTau,
+    PAbeta = colData(spe)$PAbeta,
+    PpTau = colData(spe)$PpTau
+    )
+'''
+
+
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
