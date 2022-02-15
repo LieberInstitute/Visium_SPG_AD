f88fff37518631f8ba75720ab8377422da67bc99
sparthib <sparthi1@jhu.edu>
2022-02-15 10:32:11 -0500

    Create scatter plots for n to p comparison for Abeta and pTau

--- code/07_spot_qc/compare_pathology_number_to_percent.R
+++ code/07_spot_qc/compare_pathology_number_to_percent.R
@@ -16,11 +16,13 @@ load(here::here("processed-data", "07_spot_qc", "spe_targeted_postqc.Rdata"), ve
 
 
 ## output directories
-dir_plots <- here::here("plots", "07_spot_qc")
+dir_plots <- here::here("plots", "07_spot_qc", "outliers")
+dir.create(dir_plots, showWarnings = FALSE)
 
 
-##Create function to create 2x2 Table
-two_by_two_table <- function(spe_object, sample_num, pathology, n =0, p =0.01) {
+##Create scatter plots to view outlier information
+create_plots <- function(spe_object, pathology, n = 0, p = 0.01) {
+    #pathology can be 'Abeta' or 'pTau'
     #n = threshold for number of pathology 'blobs' in spot
     #p = percentage of pathology pixels in spot
     #add optional params (diagnosis)
@@ -35,11 +37,51 @@ two_by_two_table <- function(spe_object, sample_num, pathology, n =0, p =0.01) {
         PpTau = colData(spe_object)$PpTau
     )
 
-    path_df = path_df |> filter(sample_id == sample_num)
-
-    ## 2x2 table column 1: Number present, column 2: Number absent
     ## row 1: Percent > 0.01, column 2: Percent absent
+    if(pathology == 'Abeta'){
+        path_df<-  path_df |> mutate(outliers = case_when(
+            NAbeta > n & PAbeta > p ~ "both",
+            NAbeta > n & PAbeta <= p ~ 'n',
+            NAbeta <= n & PAbeta > p ~ 'p',
+            NAbeta <= n & PAbeta <= p ~ 'none'))
 
+        plot <- ggpubr::ggscatter(path_df, x = "NAbeta", y = "PAbeta",
+                                  color = "outliers", size = 0.5)
+        plot <- facet(plot+ theme_bw(), facet.by = "sample_id",
+                      short.panel.labs = FALSE)
+
+    }
+
+    if(pathology == 'pTau'){
+        path_df<-  path_df |> mutate(outliers = case_when(
+            NpTau > n & PpTau > p ~ "both",
+            NpTau > n & PpTau <= p ~ 'n',
+            NpTau <= n & PpTau > p ~ 'p',
+            NpTau <= n & PpTau <= p ~ 'none'))
+
+        plot <- ggpubr::ggscatter(path_df, x = "NpTau", y = "PpTau",
+                                  color = "outliers", size = 0.5)
+        plot <- facet(plot+ theme_bw(), facet.by = "sample_id",
+                      short.panel.labs = FALSE)
+
+    }
+
+return(plot)
+}
+
+
+
+# create plots for whole genome
+for (path in c('Abeta', 'pTau')){
+    pdf(file.path(dir_plots, paste0("spe_whole", "_",path,"_","scatter",".pdf")), width = 14)
+    print(create_plots(spe, path))
+    dev.off()
+
+}
+
+
+
+'''
     if(pathology == 'Abeta'){
         col1_row1 = path_df |> filter(NAbeta > n & PAbeta > p) |> count()
         col1_row2 = path_df |> filter(NAbeta > n & PAbeta <= p) |> count()
@@ -64,7 +106,10 @@ two_by_two_table <- function(spe_object, sample_num, pathology, n =0, p =0.01) {
     vals = c(col1_row1, col1_row2, col2_row1, col2_row2)
     matrix_2x2 = matrix(vals, nrow = 2, ncol = 2,dimnames=list(row_names, col_names))
     return(matrix_2x2)
-}
+'''
+
+
+
 
 
 ## Reproducibility information
