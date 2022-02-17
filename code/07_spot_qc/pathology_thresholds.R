d306e45ec491335206084f032d3f906cd670a7fc
sparthib <sparthi1@jhu.edu>
2022-02-17 12:42:13 -0500

    quantiles for NAbeta and PAbeta

--- code/07_spot_qc/pathology_thresholds.R
+++ code/07_spot_qc/pathology_thresholds.R
@@ -8,7 +8,7 @@ library("ggplot2")
 library("dplyr")
 library("spatialLIBD")
 library("sessioninfo")
-
+library("tidyr")
 ## Load basic SPE data
 load(here::here("processed-data", "07_spot_qc", "spe_postqc.Rdata"), verbose = TRUE)
 
@@ -36,5 +36,45 @@ path_df |> filter(sample_id %in% controls[1:2]) |> summarise_if(is.numeric, max,
 #NAbeta NpTau    PAbeta       PpTau
 #  4     7     0.1983471      0.01396914
 
+
+
+##Quantiles for NAbeta
+path_df |> filter(sample_id %in% controls) |> group_by(sample_id) |>
+    summarise( q = list(quantile(NAbeta)), na.rm = TRUE) |> unnest_wider(q)
+
+'''
+sample_id            `0%` `25%` `50%` `75%` `100%` na.rm
+  <chr>               <dbl> <dbl> <dbl> <dbl>  <dbl> <lgl>
+1 V10A27004_A1_Br3874     0     0     0     0      4 TRUE
+2 V10A27106_A1_Br3874     0     0     0     0      2 TRUE
+3 V10T31036_A1_Br3874     0     0     0     0      3 TRUE
+'''
+
+##Frequency of unique NAbeta values across all controls
+
+path_df |> filter(sample_id %in% controls) |> count(NAbeta) |>
+    group_by(NAbeta) |> mutate(prop = prop.table(n))
+
+'''
+    NAbeta   n  prop
+   <int> <int> <dbl>
+1      0 12963     1
+2      1    22     1
+3      2     3     1
+4      3     2     1
+5      4     1     1
+'''
+
+## Quantiles for PAbeta
+path_df |> filter(sample_id %in% controls) |> group_by(sample_id) |>
+    summarise( q = list(quantile(PAbeta)), na.rm = TRUE) |> unnest_wider(q)
+
+'''
+sample_id            `0%` `25%` `50%` `75%` `100%` na.rm
+<chr>               <dbl> <dbl> <dbl> <dbl>  <dbl> <lgl>
+1 V10A27004_A1_Br3874     0     0     0     0 0.198  TRUE
+2 V10A27106_A1_Br3874     0     0     0     0 0.0649 TRUE
+3 V10T31036_A1_Br3874     0     0     0     0 0.149  TRUE
+'''
 
 
