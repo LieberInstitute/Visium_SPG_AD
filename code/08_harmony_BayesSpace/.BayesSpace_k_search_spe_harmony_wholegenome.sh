08d7747cdca831b5624404ba88ab6d3af2df3efb
lcolladotor <lcollado@jhsph.edu>
2022-02-17 12:30:44 -0500

    Re-submit k = 9 with 120 GB

--- code/08_harmony_BayesSpace/.BayesSpace_k_search_spe_harmony_wholegenome.sh
+++ code/08_harmony_BayesSpace/.BayesSpace_k_search_spe_harmony_wholegenome.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -l mem_free=120G,h_vmem=120G,h_fsize=100G
 #$ -N BayesSpace_k_search_spe_harmony_wholegenome
 #$ -o logs/BayesSpace_k_search_spe_harmony_wholegenome.$TASK_ID.txt
 #$ -e logs/BayesSpace_k_search_spe_harmony_wholegenome.$TASK_ID.txt
 #$ -m e
-#$ -t 2-15
+#$ -t 9
 #$ -tc 20
 #$ -hold_jid preprocess_and_harmony_spe_postqc
 
