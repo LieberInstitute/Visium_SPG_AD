d16cdf4280bd636769462aae7b7e156ee069ffdc
sparthib <sparthi1@jhu.edu>
2022-02-22 11:00:42 -0500

    create annotated barplots for Abeta and pTau outliers for Bayesspace clusters 4 - 15

--- code/07_spot_qc/bayesspace_pathology_barplots.R
+++ code/07_spot_qc/bayesspace_pathology_barplots.R
@@ -42,36 +42,68 @@ cluster_df <- cluster_df |> mutate(across(matches("BayesSpace_harmony"),factor )
 
 cluster_df <- cluster_df |> mutate(pTau_outliers =
                                        ifelse(NpTau > 7 & PpTau > 0.014, "outlier", "normal"))
+cluster_df <- cluster_df |> mutate(Abeta_outliers =
+                                       ifelse(NAbeta > 1 & PAbeta > 0.108, "outlier", "normal"))
 
 
+
+##pTau observations
+pTau_observations <- cluster_df |> group_by(pTau_outliers, sample_id,i ) |>
+    summarise(n=n()) |> mutate(pTau_outlier_obs = n)
 
 # Stacked bar plots, add labels inside bars
 
 plot <- ggplot(cluster_df, aes(fill = pTau_outliers,
-                       x = imported_BayesSpace_harmony_k14, y = PpTau))+
-    geom_bar(position = "fill", stat = "identity")
+                       x = imported_BayesSpace_harmony_k14))+ #, y = PpTau
+    geom_bar(position = "fill", stat = "count") +
+    geom_text(aes(label=stat(count)), stat='count', position='fill')
+
+
 
 
 
 bayes_cols <- cluster_df |> select(matches("BayesSpace_harmony"))
-
+path_cols <- cluster_df |> select(matches("_outliers"))
 ##
 
-pdf(file.path(dir_plots, paste0("spe_whole_barplots_pTau", ".pdf")), width = 14)
+
+
+pdf(file.path(dir_plots, paste0("spe_whole_barplots_pTau_outliers",".pdf")), width = 14)
 
 for (i in bayes_cols) {
-    plot <- ggplot(cluster_df, aes(x= i))+
-        geom_bar(aes(fill = pTau_outliers),
-                    position = 'fill')+
-        scale_y_continuous(labels = scales::percent)+
-        labs ( y = "Percentage")
-    plot <-facet(plot+ theme_bw(), facet.by = "sample_id",
-            short.panel.labs = FALSE)
 
+    plot <- ggplot(cluster_df, aes(x= i, fill = pTau_outliers))+
+     geom_bar(position = 'fill', stat = "count")+
+        scale_y_continuous(labels = scales::percent)+
+        labs ( y = "Percentage", x = colnames(i)) +
+        geom_text(aes(label=stat(count)), stat='count', position='fill', size = 2)
+
+
+    plot <-facet(plot+ theme_bw(), facet.by = "sample_id",
+        short.panel.labs = FALSE)
 
     print(plot)
     }
 dev.off()
+
+
+
+pdf(file.path(dir_plots, paste0("spe_whole_barplots_Abeta_outliers", ".pdf")), width = 14)
+for (i in bayes_cols) {
+    plot <- ggplot(cluster_df, aes(x= i, fill = Abeta_outliers))+
+        geom_bar(position = 'fill', stat = "count")+
+        scale_y_continuous(labels = scales::percent)+
+        labs ( y = "Percentage") +
+        geom_text(aes(label=stat(count)), stat='count', position='fill', size = 2)
+
+    plot <-facet(plot+ theme_bw(), facet.by = "sample_id",
+                 short.panel.labs = FALSE)
+
+
+    print(plot)
+}
+dev.off()
+
 
 
 
