df22a9a737ad97a96f8109a8e8b37ebf73d1beaa
sparthib <sparthi1@jhu.edu>
2022-02-27 16:08:40 -0500

    match ensembl names with gene_layer before heatmap plotting

--- code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
+++ code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
@@ -11,6 +11,9 @@ library("spatialLIBD")
 library("sessioninfo")
 library("scuttle")
 library(Matrix.utils)
+library(ComplexHeatmap)
+
+
 
 sig_genes <- read_csv('https://raw.githubusercontent.com/LieberInstitute/HumanPilot/master/Analysis/Layer_Guesses/sig_genes.csv')
 table(sig_genes$test)
@@ -32,25 +35,43 @@ spe <- cluster_import(
     cluster_dir = file.path(dir_rdata_whole, "clusters_BayesSpace"),
     prefix = "imported_")
 
+spe<- logNormCounts(spe)
+
 sig_genes <- sig_genes |> filter(test == "layer_vs_rest")
+
 counts_subset <- counts(spe)[rownames(counts(spe)) %in% sig_genes$ensembl, ]
+# counts_subset <-as.data.frame(as.matrix(counts_subset))
+sig_genes <- as.data.frame(sig_genes)
+rownames(sig_genes) <- sig_genes$ensembl
+sig_genes$gene_layer <- paste(sig_genes$gene, sig_genes$layer)
+#
+# counts_subset <- merge(x = counts_subset, y = sig_genes, by ='row.names')
+# counts_subset <- na.omit(counts_subset)
+# rownames(counts_subset) <- counts_subset$gene_layer
+# counts_subset <- counts_subset[, -which(names(counts_subset) %in% colnames(sig_genes))]
+# counts_subset<- select(counts_subset, -1)
+# counts_subset <- as(as.matrix(counts_subset), "sparseMatrix")
 
 #assay(spe, "counts_subset") <- counts_subset
 #can't add counts_subset to assays since rownames are not identical to OG
 
+
+
 ##pseudobulking
 
-
-spe_new <- SummarizedExperiment(assays = list(counts = counts_subset),
+spe_new <- SpatialExperiment(assays = list(counts = counts_subset),
                          colData = colData(spe))
-                         #rowData = rowData(spe)[colnames(rowData(spe)) %in% sig_genes$ensembl] )
 
 groups <- colData(spe_new)[, c("sample_id", "imported_BayesSpace_harmony_k15")]
 
 pb <- aggregate.Matrix(t(counts(spe_new)),
                        groupings = groups, fun = "sum")
-
+colnames(pb)
+rownames(sig_genes)
+sig_gen_index <- match(colnames(pb), rownames(sig_genes))
+reordered <- sig_genes[sig_gen_index,]
+colnames(pb) <- reordered$gene_layer
 #ave.beta <- aggregateAcrossCells(spe_new, statistics="mean",
                                  #use.assay.type="counts", ids=c(spe$sample_id, spe$imported_BayesSpace_harmony_k08))
-
+Heatmap(as.matrix(pb))
 
