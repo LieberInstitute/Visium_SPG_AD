3d5b29a006904ffb48621dffff751b85ab114df1
sparthib <sparthi1@jhu.edu>
2022-03-01 13:06:08 -0500

    code for targeted and plot. results look funny. cluster numbers in decimals.

--- code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
+++ code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
@@ -11,18 +11,29 @@ library('stringr')
 library('Matrix.utils')
 library('ComplexHeatmap')
 library('cowplot')
-library('Polychrome')
 
+
+##load and process sig_genes
 sig_genes <- read_csv('https://raw.githubusercontent.com/LieberInstitute/HumanPilot/master/Analysis/Layer_Guesses/sig_genes.csv')
 table(sig_genes$test)
 
-## Load basic SPE data
-load(here::here("processed-data", "07_spot_qc", "spe_postqc.Rdata"), verbose = TRUE)
+#filter layer-related genes
+sig_genes <- sig_genes |> filter(test == "layer_vs_rest")
+#dim(sig_genes)  70x15
 
+#create new column to merge gene name and layer
+sig_genes <- as.data.frame(sig_genes)
+rownames(sig_genes) <- sig_genes$ensembl
+sig_genes$gene_layer <- paste(sig_genes$gene, sig_genes$layer)
 
 ##output directories
 dir_plots <- here::here("plots", "07_spot_qc", "heatmaps")
 dir.create(dir_plots, showWarnings = FALSE)
+
+################################whole genome####################################
+
+## Load basic SPE data
+load(here::here("processed-data", "07_spot_qc", "spe_postqc.Rdata"), verbose = TRUE)
 
 
 #import cluster info for whole genome
@@ -36,24 +47,12 @@ spe <- cluster_import(
 #add logcounts(spe)
 spe<- logNormCounts(spe)
 
-#filter layer-related genes
-sig_genes <- sig_genes |> filter(test == "layer_vs_rest")
-#dim(sig_genes)  70x15
-
-#create new column to merge gene name and layer
-sig_genes <- as.data.frame(sig_genes)
-rownames(sig_genes) <- sig_genes$ensembl
-sig_genes$gene_layer <- paste(sig_genes$gene, sig_genes$layer)
-
-
 #only keep gene logcounts found in sig_genes
 counts_subset <- logcounts(spe)[rownames(logcounts(spe)) %in% sig_genes$ensembl, ]
 #dim(counts_subset)
 #[1]    69 38115
 #setdiff(rownames(counts_subset),sig_genes$ensembl)
 #character(0)   setdiff returns this.
-
-
 
 
 
@@ -112,7 +111,80 @@ for( k in bayes_cols){
 }
 dev.off()
 
+################################targeted genome##################################
 
+load(here::here("processed-data", "07_spot_qc", "spe_targeted_postqc.Rdata"), verbose = TRUE)
+
+#import cluster info for targeted
+dir_rdata_whole <- here::here("processed-data", "08_harmony_BayesSpace", "targeted") #, suffix
+
+spe_targeted <- cluster_import(
+    spe_targeted,
+    cluster_dir = file.path(dir_rdata_whole, "clusters_BayesSpace"),
+    prefix = "imported_")
+
+#add logcounts(spe)
+spe_targeted<- logNormCounts(spe_targeted)
+
+#only keep gene logcounts found in sig_genes
+counts_subset <- logcounts(spe_targeted)[rownames(logcounts(spe_targeted)) %in% sig_genes$ensembl, ]
+#dim(counts_subset)
+#[1] 68 38115
+#setdiff(rownames(counts_subset),sig_genes$ensembl)
+#character(0)   setdiff returns this.
+
+
+##pseudobulking
+
+spe_new <- SpatialExperiment(assays = list(counts = counts_subset),
+                             colData = colData(spe_targeted))
+
+
+bayes_cols <- colnames(as.data.frame(colData(spe_new)) |> select(matches("BayesSpace_harmony")))
+
+
+pdf(file.path(dir_plots, paste0("spe_targeted_dlpfc_heatmap", ".pdf")), width = 14)
+for( k in bayes_cols){
+    groups <- colData(spe_new)[, c("sample_id", k)]
+
+    pb <- aggregate.Matrix(t(counts(spe_new)),
+                           groupings = groups, fun = "sum")
+
+    sig_gen_index <- match(colnames(pb), rownames(sig_genes))
+    reordered <- sig_genes[sig_gen_index,]
+
+
+    pb<- as.matrix(pb)
+    pb<-as.data.frame(pb)
+    colnames(pb) <- reordered$gene_layer
+
+    pb$sample_cluster <- rownames(pb)
+    pb <-pb |> mutate(sample = str_sub(sample_cluster, 1,19))
+    pb<- pb |> mutate(cluster = str_sub(sample_cluster, 21))
+
+    s <- split(pb, pb$sample)
+
+    ## plot for k = 15
+    myplots <- list()
+
+    for(i in 1:10){
+        rownames(s[[i]]) = s[[i]]$cluster
+        plot <- grid.grabExpr(draw(Heatmap(as.matrix(s[[i]][,1:69]),
+                                           column_names_gp = grid::gpar(fontsize = 3.2),
+                                           row_names_gp = grid::gpar(fontsize = 5),
+                                           column_title = as.character(s[[i]]$sample[1]),
+                                           column_title_gp = grid::gpar(fontsize = 8),
+                                           name = " ")))
+        myplots[[i]] <- plot
+
+
+    }
+    plot_grid <- cowplot::plot_grid(plotlist = myplots, ncol =4)
+    print(plot_grid)
+}
+dev.off()
+
+################################################################################
 
 ## Reproducibility information
 print("Reproducibility information:")
