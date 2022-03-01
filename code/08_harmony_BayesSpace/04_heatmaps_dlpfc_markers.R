6c3afcf71fa45e90069c59d5bd8c4b369c2e2891
sparthib <sparthi1@jhu.edu>
2022-02-28 20:45:18 -0500

    create heatmaps for k = 2:15 with log counts, change font size

--- code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
+++ code/08_harmony_BayesSpace/04_heatmaps_dlpfc_markers.R
@@ -4,19 +4,13 @@ library('pheatmap')
 library("SpatialExperiment")
 library("scran")
 library("scater")
-library("ggpubr")
-library("ggplot2")
 library("dplyr")
 library("spatialLIBD")
 library("sessioninfo")
-library("scuttle")
 library('stringr')
-library(Matrix.utils)
-library(ComplexHeatmap)
-library(cowplot)
-
-
-
+library('Matrix.utils')
+library('ComplexHeatmap')
+library('cowplot')
 
 sig_genes <- read_csv('https://raw.githubusercontent.com/LieberInstitute/HumanPilot/master/Analysis/Layer_Guesses/sig_genes.csv')
 table(sig_genes$test)
@@ -38,30 +32,28 @@ spe <- cluster_import(
     cluster_dir = file.path(dir_rdata_whole, "clusters_BayesSpace"),
     prefix = "imported_")
 
+#add logcounts(spe)
 spe<- logNormCounts(spe)
 
+#filter layer-related genes
 sig_genes <- sig_genes |> filter(test == "layer_vs_rest")
+#dim(sig_genes)  70x15
 
-counts_subset <- counts(spe)[rownames(counts(spe)) %in% sig_genes$ensembl, ]
-# counts_subset <-as.data.frame(as.matrix(counts_subset))
+#create new column to merge gene name and layer
 sig_genes <- as.data.frame(sig_genes)
 rownames(sig_genes) <- sig_genes$ensembl
 sig_genes$gene_layer <- paste(sig_genes$gene, sig_genes$layer)
-#
-# counts_subset <- merge(x = counts_subset, y = sig_genes, by ='row.names')
-# counts_subset <- na.omit(counts_subset)
-# rownames(counts_subset) <- counts_subset$gene_layer
-# counts_subset <- counts_subset[, -which(names(counts_subset) %in% colnames(sig_genes))]
-# counts_subset<- select(counts_subset, -1)
-# counts_subset <- as(as.matrix(counts_subset), "sparseMatrix")
-
-#assay(spe, "counts_subset") <- counts_subset
-#can't add counts_subset to assays since rownames are not identical to OG
 
 
-rownames(sig_genes)
-sig_gen_index <- match(colnames(pb), rownames(sig_genes))
-reordered <- sig_genes[sig_gen_index,]
+#only keep gene logcounts found in sig_genes
+counts_subset <- logcounts(spe)[rownames(logcounts(spe)) %in% sig_genes$ensembl, ]
+#dim(counts_subset)
+#[1]    69 38115
+#setdiff(rownames(counts_subset),sig_genes$ensembl)
+#character(0)   setdiff returns this.
+
+
+
 
 
 ##pseudobulking
@@ -70,38 +62,54 @@ spe_new <- SpatialExperiment(assays = list(counts = counts_subset),
                          colData = colData(spe))
 
 
-groups <- colData(spe_new)[, c("sample_id", "imported_BayesSpace_harmony_k15")]
+#check if BayesSpace columns have NA vals
+#sum(is.na(as.data.frame(colData(spe_new)) |> select(matches("BayesSpace_harmony"))))
+#returns 0
 
-pb <- aggregate.Matrix(t(counts(spe_new)),
+
+bayes_cols <- colnames(as.data.frame(colData(spe_new)) |> select(matches("BayesSpace_harmony")))
+
+
+pdf(file.path(dir_plots, paste0("spe_whole_dlpfc_heatmap", ".pdf")), width = 14)
+for( k in bayes_cols){
+    groups <- colData(spe)[, c("sample_id", k)]
+
+    pb <- aggregate.Matrix(t(counts(spe_new)),
                        groupings = groups, fun = "sum")
 
-pb<- as.matrix(pb)
-pb<-as.data.frame(pb)
-colnames(pb) <- reordered$gene_layer
-
-pb$sample_cluster <- rownames(pb)
-pb <-pb |> mutate(sample = str_sub(sample_cluster, 1,19))
-pb<- pb |> mutate(cluster = str_sub(sample_cluster, 21))
-
-s <- split(pb, pb$sample)
+    sig_gen_index <- match(colnames(pb), rownames(sig_genes))
+    reordered <- sig_genes[sig_gen_index,]
 
 
-## plot for k = 15
-myplots <- list()
+    pb<- as.matrix(pb)
+    pb<-as.data.frame(pb)
+    colnames(pb) <- reordered$gene_layer
 
-for(i in 1:10){
-rownames(s[[i]]) = s[[i]]$cluster
-plot <- grid.grabExpr(draw(Heatmap(as.matrix(s[[i]][,1:69]),
-        column_names_gp = grid::gpar(fontsize = 4),
-        row_names_gp = grid::gpar(fontsize = 6),
-        column_title = as.character(s[[i]]$sample[1]),
-        column_title_gp = grid::gpar(fontsize = 8),
-        name = " ")))
-myplots[[i]] <- plot
+    pb$sample_cluster <- rownames(pb)
+    pb <-pb |> mutate(sample = str_sub(sample_cluster, 1,19))
+    pb<- pb |> mutate(cluster = str_sub(sample_cluster, 21))
+
+    s <- split(pb, pb$sample)
+
+    ## plot for k = 15
+    myplots <- list()
+
+    for(i in 1:10){
+        rownames(s[[i]]) = s[[i]]$cluster
+        plot <- grid.grabExpr(draw(Heatmap(as.matrix(s[[i]][,1:69]),
+                                           column_names_gp = grid::gpar(fontsize = 3.2),
+                                           row_names_gp = grid::gpar(fontsize = 5),
+                                           column_title = as.character(s[[i]]$sample[1]),
+                                           column_title_gp = grid::gpar(fontsize = 8),
+                                           name = " ")))
+        myplots[[i]] <- plot
 
 
+    }
+    plot_grid <- cowplot::plot_grid(plotlist = myplots, ncol =4)
+    print(plot_grid)
 }
-cowplot::plot_grid(plotlist = myplots, ncol =4)
+dev.off()
 
 
 
