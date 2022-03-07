c297900d10843e0cfb6f31e1019cf4b3ccce5de6
lcolladotor <lcollado@jhsph.edu>
2022-03-07 14:07:58 -0500

    Removed some core dump files. Version control hidden scripts by Sowmya.
    Example core dump file contents:
    
    ```bash
    $ gdb core.5554
    GNU gdb (GDB) Red Hat Enterprise Linux 7.6.1-120.el7
    Copyright (C) 2013 Free Software Foundation, Inc.
    License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
    This is free software: you are free to change and redistribute it.
    There is NO WARRANTY, to the extent permitted by law.  Type "show copying"
    and "show warranty" for details.
    This GDB was configured as "x86_64-redhat-linux-gnu".
    For bug reporting instructions, please see:
    <http://www.gnu.org/software/gdb/bugs/>...
    [New LWP 5554]
    [New LWP 5564]
    Core was generated by `/jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/R/4.1.x/lib64/'.
    Program terminated with signal 7, Bus error.
    "/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/08_harmony_BayesSpace/core.5554" is a core file.
    Please specify an executable to debug.
    (gdb) quit
    ```
    
    Co-authored-by: Sowmya Parthiban <sparthi1@jhu.edu>

---
+++ code/08_harmony_BayesSpace/.05_fastplus_optimal_k_targeted.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=80G,h_vmem=80G,h_fsize=100G
+#$ -N 05_fastplus_optimal_k_targeted
+#$ -o logs/05_fastplus_optimal_k_targeted.$TASK_ID.txt
+#$ -e logs/05_fastplus_optimal_k_targeted.$TASK_ID.txt
+#$ -m e
+#$ -t 2-15
+#$ -tc 14
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript -e "options(width = 120); print('targeted'); sessioninfo::session_info()"
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
+
