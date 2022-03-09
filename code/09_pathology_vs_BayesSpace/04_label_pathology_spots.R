39eb90fe73fab4eb9410ae50c953d60426ad97fb
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-03-08 20:30:26 -0500

    Implemented the alternative scenario where Abeta+ gets prioritized
    Related to #25
    
    * pTau+_Abeta+ (aka "both") becomes pTau-_Abeta+ (aka "Ab+"), so Abeta takes priority over pTau
    * Neighboring spots for "Ab+" take priority over "pT+"
    * Original "next_both" stops remain untouched

--- code/09_pathology_vs_BayesSpace/04_label_pathology_spots.R
+++ code/09_pathology_vs_BayesSpace/04_label_pathology_spots.R
@@ -126,6 +126,64 @@ for (i in colnames(colData(spe))[grep("^path_", colnames(colData(spe)))]) {
     )
 }
 
+
+## Remove the "both" category in this alternative and prioritize Abeta+ neighbors
+## over pTau+
+spe$path_groups <- gsub("au|eta", "", paste0(spe$path_pTau, "_", spe$path_Abeta))
+table(spe$path_groups)
+# pT-_Ab- pT-_Ab+ pT+_Ab- pT+_Ab+
+#   27140    1279    8960     736
+## Key change number 1: prioritze Abeta+ over pTau+
+spe$path_groups[spe$path_groups == "pT+_Ab+"] <- "pT-_Ab+"
+table(spe$path_groups)
+# pT-_Ab- pT-_Ab+ pT+_Ab-
+#   27140    2015    8960
+
+has_path <- which(spe$path_pTau == "pTau+" | spe$path_Abeta == "Abeta+")
+## Make has_path into an argument in this version
+which_neighbors <- function(var, values, has_path) {
+    i <- which(colData(spe)[[var]] %in% values)
+    ## Undo the C++ index from
+    ## https://github.com/edward130603/BayesSpace/blob/master/R/spatialCluster.R#L231
+    res <- sort(unique(unlist(neighbors_list[i]))) + 1
+
+    ## Prioritize the initial pathology categories
+    res[!res %in% has_path]
+}
+
+path_neighbor_pTau <- which_neighbors("path_pTau", "pTau+", has_path)
+
+## Key change number 2: prioritize Abeta+ neighbors over pTau+
+path_neighbor_Abeta <- which_neighbors("path_Abeta", "Abeta+", which(spe$path_Abeta == "Abeta+"))
+path_neighbor_both <- intersect(path_neighbor_Abeta, path_neighbor_pTau)
+
+## Add the neighbors
+spe$path_groups[setdiff(path_neighbor_pTau, path_neighbor_both)] <- "next_pT+"
+spe$path_groups[setdiff(path_neighbor_Abeta, path_neighbor_both)] <- "next_Ab+"
+spe$path_groups[path_neighbor_both] <- "next_both"
+
+## Simplify some groups even more
+spe$path_groups <- gsub("pT-_Ab\\+", "Ab+", gsub("pT\\+_Ab-", "pT+", gsub("pT-_Ab-", "none", spe$path_groups)))
+
+spe$path_groups <- factor(spe$path_groups, levels = c("none", "Ab+", "next_Ab+", "pT+", "next_pT+", "next_both"))
+addmargins(table(spe$path_groups))
+#  none       Ab+  next_Ab+       pT+  next_pT+ next_both       Sum
+# 19244      2015      5645      6381      3638      1192     38115
+round(addmargins(table(spe$path_groups)) / ncol(spe) * 100, 2)
+# none       Ab+  next_Ab+       pT+  next_pT+ next_both       Sum
+# 50.49      5.29     14.81     16.74      9.54      3.13    100.00
+
+
+vis_grid_clus(
+    spe = spe,
+    clustervar = "path_groups",
+    pdf_file = file.path(dir_plots, "pathology_groups_alternative.pdf"),
+    sort_clust = FALSE,
+    colors = colors_pathology[names(colors_pathology) %in% levels(spe$path_groups)],
+    spatial = FALSE,
+    point_size = 2
+)
+
 ## Reproducibility information
 print("Reproducibility information:")
 Sys.time()
