0ca5f5bf1824db0f9e74caacbca5dfc2e520aa90
sparthib <sparthi1@jhu.edu>
2022-03-14 21:12:36 -0400

    pseudo bulk, can't perform logNormCounts. Negative size factors? range is (-inf, inf)

--- code/09_pathology_vs_BayesSpace/06_SpatialRegistration.R
+++ code/09_pathology_vs_BayesSpace/06_SpatialRegistration.R
@@ -21,4 +21,38 @@ spe <-
         )
     )
 
+spe <- cluster_import(
+    spe,
+    cluster_dir = here::here("processed-data",
+                             "08_harmony_BayesSpace",
+                             "wholegenome",
+                             "clusters_BayesSpace"),
+    prefix = ""
+)
+
+
+## Pseudo-bulk for our current BayesSpace cluster results
+spe_pseudo <- aggregateAcrossCells(
+    spe,
+    DataFrame(
+        BayesSpace = colData(spe)[[k]],
+        sample_id = spe$sample_id
+    )
+)
+
+#spe_pseudo <- logNormCounts(spe_pseudo) #size factors <0?
+
+# range(spe$size_factor)
+# [1]  Inf -Inf
+# spe$size_factor[spe$size_factor =='NA'] #size factors
+# NULL
+#
+
+
+mat <- counts(spe_pseudo)
+
+# remove lowly expressed genes?
+# gIndex = rowMeans(mat) > 0.2
+# mat_filter = mat[gIndex, ]
+
 
