ad1bf523bcbfbce12c0ffcd6f69220ee36f794e6
sparthib <sparthi1@jhu.edu>
2022-03-17 14:25:10 -0400

    add notes on block, change subject to sample and reformat

--- code/10_spatial_registration/01_spatial_registration.R
+++ code/10_spatial_registration/01_spatial_registration.R
@@ -10,6 +10,7 @@
 
 
 
+
 library(SpatialExperiment)
 library(here)
 library(spatialLIBD)
@@ -43,10 +44,12 @@ spe <-
 
 spe <- cluster_import(
     spe,
-    cluster_dir = here::here("processed-data",
-                             "08_harmony_BayesSpace",
-                             "wholegenome",
-                             "clusters_BayesSpace"),
+    cluster_dir = here::here(
+        "processed-data",
+        "08_harmony_BayesSpace",
+        "wholegenome",
+        "clusters_BayesSpace"
+    ),
     prefix = ""
 )
 
@@ -61,15 +64,15 @@ spe <- cluster_import(
 # )
 
 
-spe$PseudoSample = paste0(spe$sample_id, ":",colData(spe)[[paste0("BayesSpace_harmony_k",k_nice)]])
+spe$PseudoSample = paste0(spe$sample_id, ":", colData(spe)[[paste0("BayesSpace_harmony_k", k_nice)]])
 cIndexes = splitit(spe$PseudoSample) # gives you the index for each cluster
 
 umiComb <- sapply(cIndexes, function(ii)
     rowSums(assays(spe)$counts[, ii, drop = FALSE])) #
 
-phenoComb = colData(spe)[!duplicated(spe$PseudoSample),] #creates new colData dataframe with pseudobulked colData
+phenoComb = colData(spe)[!duplicated(spe$PseudoSample), ] #creates new colData dataframe with pseudobulked colData
 rownames(phenoComb) = phenoComb$PseudoSample #renames rows of new colData frame to be the clusters
-phenoComb = phenoComb[colnames(umiComb), ]
+phenoComb = phenoComb[colnames(umiComb),]
 phenoComb = DataFrame(phenoComb)
 
 sce_pseudo <-
@@ -86,7 +89,15 @@ sce_pseudo <-
 # NULL
 #
 
-saveRDS(sce_pseudo, file = here::here("processed-data","10_spatial_registration", "pseudo_bulked",paste0("sce_pseudobulked_BayesSpace",k_nice,".RDS")))
+saveRDS(
+    sce_pseudo,
+    file = here::here(
+        "processed-data",
+        "10_spatial_registration",
+        "pseudo_bulked",
+        paste0("sce_pseudobulked_BayesSpace", k_nice, ".RDS")
+    )
+)
 
 ###############################
 ##### get mean expression  ####
@@ -94,14 +105,15 @@ mat <- logcounts(sce_pseudo)
 
 ## filter
 gIndex = rowMeans(mat) > 0.2 # find the genes for which the mean expression is greater than 0.2
-mat_filter = mat[gIndex, ] #subset matrix on just those genes.  want to remove lowly expressed genes.
+mat_filter = mat[gIndex,] #subset matrix on just those genes.  want to remove lowly expressed genes.
 
 
 #####################
 ## Build a group model
 
 #convert variables to factors
-sce_pseudo$spatial.cluster <- as.factor(colData(sce_pseudo)[[paste0("BayesSpace_harmony_k",k_nice)]]) #NAs present
+sce_pseudo$spatial.cluster <-
+    as.factor(colData(sce_pseudo)[[paste0("BayesSpace_harmony_k", k_nice)]]) #NAs present
 sce_pseudo$age <- as.integer(sce_pseudo$age)
 sce_pseudo$sex <- as.factor(sce_pseudo$sex)
 sce_pseudo$diagnosis <- as.factor(sce_pseudo$diagnosis)
@@ -110,34 +122,56 @@ sce_pseudo$subject <- as.factor(sce_pseudo$subject)
 
 
 mod <- with(colData(sce_pseudo),
-            model.matrix(~ 0 + spatial.cluster + age + sex +diagnosis))
+            model.matrix( ~ 0 + spatial.cluster + age + sex + diagnosis))
 colnames(mod) <- gsub('cluster', '', colnames(mod)) #not neccesary
 
 ## get duplicate correlation
 corfit <- duplicateCorrelation(mat_filter, mod,
                                block = sce_pseudo$subject)
-saveRDS(corfit, file = here::here("processed-data","10_spatial_registration", "dupCor",paste0("pseudobulked_dupCor_k",k_nice,".RDS")))
+saveRDS(
+    corfit,
+    file = here::here(
+        "processed-data",
+        "10_spatial_registration",
+        "dupCor",
+        paste0("pseudobulked_dupCor_k", k_nice, ".RDS")
+    )
+)
 
 
 ## Next for each layer test that layer vs the rest
-cell_idx <- splitit(sce_pseudo$spatial.cluster)  #cell_idx is actually cluster_idx
+cell_idx <-
+    splitit(sce_pseudo$spatial.cluster)  #cell_idx is actually cluster_idx
 
 eb0_list_cell <- lapply(cell_idx, function(x) {
     res <- rep(0, ncol(sce_pseudo))
-    res[x] <- 1   #indicator of whether pseudobulked column belongs to
-    m <- with(colData(sce_pseudo), #find genes that are diff expressed across diff BayesSpace clusters adjusting for
-              model.matrix(~ res + diagnosis + age + sex))      #age, diagnosis, sex
+    res[x] <-
+        1   #indicator of whether pseudobulked column belongs to
+    m <-
+        with(colData(sce_pseudo),
+             #find genes that are diff expressed across diff BayesSpace clusters adjusting for
+             model.matrix( ~ res + diagnosis + age + sex))      #age, diagnosis, sex
     eBayes(
         lmFit(
             mat_filter,
             design = m,
-            block = sce_pseudo$subject,
+            block = sce_pseudo$sample_id,
+            ##block helps take into account that there are some bulks that come from the same sample and
+            ##they may be highly correlated to each other
             correlation = corfit$consensus.correlation
         )
     )
 })
 
-saveRDS(eb0_list_cell, file = here::here("processed-data","10_spatial_registration", "specific_Ts",paste0("pseudobulked_specific_Ts_k",k_nice,".RDS")))
+saveRDS(
+    eb0_list_cell,
+    file = here::here(
+        "processed-data",
+        "10_spatial_registration",
+        "specific_Ts",
+        paste0("pseudobulked_specific_Ts_k", k_nice, ".RDS")
+    )
+)
 
 
 ##########
@@ -171,8 +205,12 @@ data.frame(
 # 4      0           0           0
 
 ###################
-load("/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/Layer_Guesses/rda/eb_contrasts.Rdata")
-load("/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/Layer_Guesses/rda/eb0_list.Rdata")
+load(
+    "/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/Layer_Guesses/rda/eb_contrasts.Rdata"
+)
+load(
+    "/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/Layer_Guesses/rda/eb0_list.Rdata"
+)
 
 ## Extract the p-values
 pvals0_contrasts <- sapply(eb0_list, function(x) {
@@ -193,14 +231,14 @@ rownames(t0_contrasts) = rownames(eb_contrasts)
 mm = match(rownames(pvals0_contrasts), rownames(pvals0_contrasts_cell))
 
 #subset data from Maynard 2021
-pvals0_contrasts = pvals0_contrasts[!is.na(mm), ]
-t0_contrasts = t0_contrasts[!is.na(mm), ]
-fdrs0_contrasts = fdrs0_contrasts[!is.na(mm), ]
+pvals0_contrasts = pvals0_contrasts[!is.na(mm),]
+t0_contrasts = t0_contrasts[!is.na(mm),]
+fdrs0_contrasts = fdrs0_contrasts[!is.na(mm),]
 
 #subset from current data
-pvals0_contrasts_cell = pvals0_contrasts_cell[mm[!is.na(mm)], ]
-t0_contrasts_cell = t0_contrasts_cell[mm[!is.na(mm)], ]
-fdrs0_contrasts_cell = fdrs0_contrasts_cell[mm[!is.na(mm)], ]
+pvals0_contrasts_cell = pvals0_contrasts_cell[mm[!is.na(mm)],]
+t0_contrasts_cell = t0_contrasts_cell[mm[!is.na(mm)],]
+fdrs0_contrasts_cell = fdrs0_contrasts_cell[mm[!is.na(mm)],]
 
 #correlation b/w t-stats
 cor_t = cor(t0_contrasts_cell, t0_contrasts)
@@ -215,15 +253,15 @@ as.data.frame(pvals0_contrasts))
 layer_ind = unique(as.numeric(layer_specific_indices))
 
 #corr matrix for t-stats with Top 100 x 7 layers
-cor_t_layer = cor(t0_contrasts_cell[layer_ind, ],
-                  t0_contrasts[layer_ind, ])
+cor_t_layer = cor(t0_contrasts_cell[layer_ind,],
+                  t0_contrasts[layer_ind,])
 signif(cor_t_layer, 3)
 
 ### heatmap
 theSeq = seq(-.85, .85, by = 0.01)
 my.col <- colorRampPalette(brewer.pal(7, "PRGn"))(length(theSeq))
 
-dd = dist(1-cor_t_layer)
+dd = dist(1 - cor_t_layer)
 hc = hclust(dd)
 cor_t_layer_toPlot = cor_t_layer[hc$order, c(1, 7:2)]
 colnames(cor_t_layer_toPlot) = gsub("ayer", "", colnames(cor_t_layer_toPlot))
@@ -236,7 +274,18 @@ dir.create(dir_plots, showWarnings = FALSE)
 
 #http://research.libd.org/spatialLIBD/reference/layer_stat_cor_plot.html newer function for plotting
 
-pdf(file = here::here("plots","10_spatial_registration",paste0("pseudobulked_bayesSpace_vs_mannual_annotations_k",k_nice,".pdf")), width = 8)
+pdf(
+    file = here::here(
+        "plots",
+        "10_spatial_registration",
+        paste0(
+            "pseudobulked_bayesSpace_vs_mannual_annotations_k",
+            k_nice,
+            ".pdf"
+        )
+    ),
+    width = 8
+)
 print(
     levelplot(
         cor_t_layer_toPlot,
