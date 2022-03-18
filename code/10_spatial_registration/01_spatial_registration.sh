30e1cc9e58dec6e54684dbcc6772c8355247a74c
sparthib <sparthi1@jhu.edu>
2022-03-17 23:38:23 -0400

    change -o

--- code/10_spatial_registration/01_spatial_registration.sh
+++ code/10_spatial_registration/01_spatial_registration.sh
@@ -2,8 +2,8 @@
 #$ -cwd
 #$ -l bluejay,mem_free=10G,h_vmem=10G,h_fsize=100G
 #$ -N spatial_registration  #delete numbers here
-#$ -o logs/01_spatial_registration.txt
-#$ -e logs/01_spatial_registration.txt
+#$ -o logs/01_spatial_registration.$TASK_ID.txt
+#$ -e logs/01_spatial_registration$TASK_ID.txt
 #$ -m e
 #$ -t 1
 #$ -tc 1
