1bddc22ee21990fa14dc6c4616bac9bea39bb7ac
Sowmya Parthiban <sparthi1@jh.edu>
2022-04-16 20:46:51 -0400

    was exploring getopt arguments

--- code/10_spatial_registration/01_spatial_registration.R
+++ code/10_spatial_registration/01_spatial_registration.R
@@ -11,22 +11,22 @@
 #
 # )
 
-# # Required libraries
-# library("getopt")
-#
-# ## Specify parameters
-# spec <- matrix(c(
-#     "spetype", "s", 2, "character", "SPE type: wholegenome or targeted",
-#     "help", "h", 0, "logical", "Display help"
-# ), byrow = TRUE, ncol = 5)
-# opt <- getopt(spec)
-#
-# ## if help was asked for print a friendly message
-# ## and exit with a non-zero error code
-# if (!is.null(opt$help)) {
-#     cat(getopt(spec, usage = TRUE))
-#     q(status = 1)
-# }
+# Required libraries
+library("getopt")
+
+## Specify parameters
+spec <- matrix(c(
+    "spetype", "s", 2, "character", "SPE type: wholegenome or targeted",
+    "help", "h", 0, "logical", "Display help"
+), byrow = TRUE, ncol = 5)
+opt <- getopt(spec)
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if (!is.null(opt$help)) {
+    cat(getopt(spec, usage = TRUE))
+    q(status = 1)
+}
 
 
 
