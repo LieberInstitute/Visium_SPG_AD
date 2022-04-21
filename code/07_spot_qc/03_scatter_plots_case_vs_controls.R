507e8ea560b038a4eb01f6d3a2885190f4a524e3
Sowmya Parthiban <sparthi1@jh.edu>
2022-04-21 08:01:30 -0400

    remove job loop
    remove getopt lines
    
    move spe creation and path_df out of for loop
    
    factorize outliers columns for Abeta and pTau
    
    add code for saving plots
    
    add reproducibility info code

--- code/07_spot_qc/03_scatter_plots_case_vs_controls.R
+++ code/07_spot_qc/03_scatter_plots_case_vs_controls.R
@@ -1,12 +1,10 @@
-# library(sgejobs)
-# sgejobs::job_loop(
-#    loops = list(spetype = c(
-#        "wholegenome", "targeted"
-#     )),
-#     name = "03_scatter_plots_case_vs_controls",
-#     create_shell = TRUE,
-#     queue = "bluejay",
-#     memory = "10G")
+library(sgejobs)
+sgejobs::job_single(
+    name = "03_scatter_plots_case_vs_controls",
+    create_shell = TRUE,
+    queue = "bluejay",
+    command = "Rscript 03_scatter_plots_case_vs_controls.R",
+    memory = "10G")
 
 library("here")
 library("SpatialExperiment")
@@ -19,88 +17,101 @@ library("spatialLIBD")
 library("sessioninfo")
 library("getopt")
 
-## Specify parameters
-spec <- matrix(c(
-    "speopt$spetype", "s", 2, "character", "SPE opt$spetype: wholegenome or targeted",
-    "help", "h", 0, "logical", "Display help"
-), byrow = TRUE, ncol = 5)
-opt <- getopt(spec = spec)
-
-## if help was asked for print a friendly message
-## and exit with a non-zero error code
-if (!is.null(opt$help)) {
-    cat(getopt(spec, usage = TRUE))
-    q(status = 1)
-}
-
-
 
 ## output plots directory
-
 # dir.create(here::here("plots","07_spot_qc", "outliers" type), showWarnings = FALSE)
 dir_plots <- here::here("plots", "07_spot_qc", "outliers")
+
+spe <-
+    readRDS(
+        here::here(
+            "processed-data",
+            "08_harmony_BayesSpace",
+            "wholegenome",
+            paste0("spe_harmony_", opt$spetype, ".rds")
+        )
+    )
+
+spe <- cluster_import(
+    spe,
+    cluster_dir = here::here(
+        "processed-data",
+        "08_harmony_BayesSpace",
+        "wholegenome",
+        "clusters_BayesSpace"
+    ),
+    prefix = ""
+)
+
+
+path_df <- data.frame(
+    spot_id = rownames(colData(spe)),
+    diagnosis = colData(spe)$diagnosis,
+    sample_id = colData(spe)$sample_id,
+    NAbeta = as.integer(colData(spe)$NAbeta),
+    NpTau = as.integer(colData(spe)$NpTau),
+    PAbeta = colData(spe)$PAbeta,
+    PpTau = colData(spe)$PpTau
+)
+
+n_Abeta = 1
+p_Abeta = 0.108
+
+path_df <- path_df |> mutate(Abeta_outliers = case_when(
+    NAbeta > n_Abeta& PAbeta > p_Abeta~ "n and %",
+    NAbeta > n_Abeta& PAbeta <= p_Abeta~ "n",
+    NAbeta <= n_Abeta& PAbeta > p_Abeta~ "%",
+    NAbeta <= n_Abeta& PAbeta <= p_Abeta~ "none"
+))
+
+n_pTau= 8
+p_pTau = 0.0143
+
+path_df <- path_df |> mutate(pTau_outliers = case_when(
+    NpTau > n_pTau& PpTau > p_pTau~ "n and %",
+    NpTau > n_pTau& PpTau <= p_pTau~ "n",
+    NpTau <= n_pTau& PpTau > p_pTau~ "%",
+    NpTau <= n_pTau& PpTau <= p_pTau~ "none"
+))
+
+path_df$diagnosis <- factor(path_df$diagnosis, levels = c("Control", "AD"))
+path_df$Abeta_outliers <- factor(path_df$Abeta_outliers ,
+                                 levels = c("n", "%", "n and %", "none"))
+path_df$pTau_outliers <- factor(path_df$pTau_outliers ,
+                                levels = c("n", "%", "n and %", "none"))
 
 
 
 ## Create scatter plots to view outlier information
-create_plots <- function(spe_object, pathology) {
+create_plots <- function(pathology) {
     # pathology can be 'Abeta' or 'pTau'
-    # n = threshold for number of pathology 'blobs' in spot
-    # p = percentage of pathology pixels in spot
-    # add optional params (diagnosis)
     colors_hex <- c("#00AFBB", "#E7B800", "#FC4E07", "#CC79A7")
 
-    path_df <- data.frame(
-        spot_id = rownames(colData(spe_object)),
-        diagnosis = colData(spe_object)$diagnosis,
-        sample_id = colData(spe_object)$sample_id,
-        NAbeta = as.integer(colData(spe_object)$NAbeta),
-        NpTau = as.integer(colData(spe_object)$NpTau),
-        PAbeta = colData(spe_object)$PAbeta,
-        PpTau = colData(spe_object)$PpTau
-    )
 
-    path_df$diagnosis <- factor(path_df$diagnosis, levels = c("Control", "AD"))
-    ## row 1: Percent > 0.01, column 2: Percent absent
     if (pathology == "Abeta") {
-        n = 1
-        p = 0.108
-        path_df <- path_df |> mutate(outliers = case_when(
-            NAbeta > n & PAbeta > p ~ "both",
-            NAbeta > n & PAbeta <= p ~ "n",
-            NAbeta <= n & PAbeta > p ~ "p",
-            NAbeta <= n & PAbeta <= p ~ "none"
-        ))
 
         plot <- ggpubr::ggscatter(path_df,
             x = "NAbeta", y = "PAbeta",
-            color = "outliers", size = 0.5, alpha = 0.3
-            # palette = c("#00AFBB", "#E7B800", "#FC4E07", "#CC79A7")
+            color = "Abeta_outliers", size = 0.5,
+            xlab = "Number of Abeta per spot (n)",
+            ylab = "Percentage of Abeta per spot (%)"
+
         )
         plot <- facet(plot + theme_bw(),
             facet.by = "diagnosis",
             short.panel.labs = TRUE
         )
         plot <- plot + scale_color_manual(
-            name = "type of Abeta",
-            labels = c("n and %", "n", "p" ,"none"),
-            values = colors_hex
-        )
+           name = "",
+           values = colors_hex)
     }
 
     if (pathology == "pTau") {
-        n= 8
-        p = 0.0143
-        path_df <- path_df |> mutate(outliers = case_when(
-            NpTau > n & PpTau > p ~ "both",
-            NpTau > n & PpTau <= p ~ "n",
-            NpTau <= n & PpTau > p ~ "p",
-            NpTau <= n & PpTau <= p ~ "none"
-        ))
-
         plot <- ggpubr::ggscatter(path_df,
             x = "NpTau", y = "PpTau",
-            color = "outliers", size = 0.5, alpha = 0.3
+            color = "pTau_outliers", size = 0.5,
+            xlab = "Number of pTau per spot (n)",
+            ylab = "Percentage of pTau per spot (%)"
         )
 
         plot <- facet(plot + theme_bw(),
@@ -109,8 +120,7 @@ create_plots <- function(spe_object, pathology) {
         )
 
         plot <- plot + scale_color_manual(
-            name = "type of pTau",
-            labels = c("n and %", "n", "p" ,"none"),
+            name = "",
             values = colors_hex
         )
     }
@@ -120,29 +130,16 @@ create_plots <- function(spe_object, pathology) {
 
 
 
-for (type in genome_type) {
-    spe <-
-        readRDS(
-            here::here(
-                "processed-data",
-                "08_harmony_BayesSpace",
-                type,
-                paste0("spe_harmony_", type, ".rds")
-            )
-        )
-
-    spe <- cluster_import(
-        spe,
-        cluster_dir = here::here(
-            "processed-data",
-            "08_harmony_BayesSpace",
-            type,
-            "clusters_BayesSpace"
-        ),
-        prefix = ""
-    )
+for (path in c("Abeta", "pTau")) {
+    pdf(file.path(dir_plots, paste0("case_vs_control_", path, "_", "scatter", ".pdf")), width = 14)
+    print(create_plots(path))
+    dev.off()
 }
 
 
-create_plots(spe, "Abeta")
-create_plots(spe, "pTau")
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
