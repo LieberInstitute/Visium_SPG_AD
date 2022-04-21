5c2ab5056024d5798993478077ba6430a323c4ad
Sowmya Parthiban <sparthi1@jh.edu>
2022-04-21 07:58:04 -0400

    remove jobloop
    remove getopt lines
    
    move spe creation and path_df out of for loop
    
    factorize outliers columns for Abeta and pTau
    
    add code for saving plots
    
    add reproducibility info code

--- code/07_spot_qc/03_scatter_plots_case_vs_controls.sh
+++ code/07_spot_qc/03_scatter_plots_case_vs_controls.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
-
-## Usage:
-# sh 03_scatter_plots_case_vs_controls.sh
-
-## Create the logs directory
-mkdir -p logs
-
-for spetype in "wholegenome"  "targeted"; do
-
-    ## Internal script name
-    SHORT="scatter_plots_case_vs_controls_${spetype}"
-
-    # Construct shell file
-    echo "Creating script 03_scatter_plots_case_vs_controls_${spetype}"
-    cat > .${SHORT}.sh <<EOF
-#!/bin/bash
 #$ -cwd
 #$ -l bluejay,mem_free=10G,h_vmem=10G,h_fsize=100G
-#$ -N ${SHORT}
-#$ -o logs/${SHORT}.txt
-#$ -e logs/${SHORT}.txt
+#$ -N scatter_plots_case_vs_controls
+#$ -o logs/scatter_plots_case_vs_controls.txt
+#$ -e logs/scatter_plots_case_vs_controls.txt
 #$ -m e
 
 echo "**** Job starts ****"
 date
 
 echo "**** JHPCE info ****"
-echo "User: \${USER}"
-echo "Job id: \${JOB_ID}"
-echo "Job name: \${JOB_NAME}"
-echo "Hostname: \${HOSTNAME}"
-echo "Task id: \${SGE_TASK_ID}"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
 
 ## Load the R module (absent since the JHPCE upgrade to CentOS v7)
 module load conda_R/devel
@@ -39,17 +23,11 @@ module load conda_R/devel
 module list
 
 ## Edit with your job command
-Rscript 03_scatter_plots_case_vs_controls.R -s ${spetype}
+Rscript 03_scatter_plots_case_vs_controls.R
+
 echo "**** Job ends ****"
 date
 
 ## This script was made using sgejobs version 0.99.1
 ## available from http://research.libd.org/sgejobs/
 
-
-EOF
-
-    call="qsub .${SHORT}.sh"
-    echo $call
-    $call
-done
