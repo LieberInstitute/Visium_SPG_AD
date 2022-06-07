6e5f475f375eb1785ae41851c1eb6f2284bf5774
Sowmya Parthiban <sparthib@compute-127.cm.cluster>
2022-06-07 16:53:46 -0400

    rename 12_magma to magma

---
+++ code/magma/03_gene_set_pvalue_100.R
@@ -0,0 +1,57 @@
+
+## load library
+library("spatialLIBD")
+library("scater") ## to compute some reduced dimensions
+library("dplyr")
+library("tidyr")
+
+##Load wholegenome data
+
+dir_rdata <- here::here(
+        "processed-data",
+        "11_grey_matter_only",
+        "without_Br3873")
+
+load(file.path(dir_rdata,
+               "wholegenome",
+               "Visium_IF_AD_modeling_results.Rdata"), verbose = TRUE)
+
+## extract gene name, pvalues
+pvalues <- as_tibble(modeling_results$enrichment) |>
+    select(starts_with('p_val') |
+    contains('gene'))
+
+##fix column names
+colnames(pvalues) <- gsub(
+    "p_value_",
+    "",
+    colnames(pvalues)
+)
+
+colnames(pvalues) <- gsub(
+    "+",
+    "",
+    colnames(pvalues),
+    fixed = T)
+
+## apply pivot longer
+pvalues <- pvalues|>
+    pivot_longer(!gene, names_to = "pathology_type",
+                 values_to = "pvalue")
+
+## group by pathology and filter top 100 smallest values
+pvalue_top_100 <- pvalues |>
+    arrange(pvalue) |>
+    group_by(pathology_type) |>
+    slice(1:100)
+
+##rearrange and drop p-value column
+pvalue_top_100 <- pvalue_top_100[, c(2,1)]
+
+##rename columns as Set and Gene
+colnames(pvalue_top_100) <- c("Set", "Gene")
+
+## write out
+write.table(pvalue_top_100, file=here::here("code","12_magma",
+                                      "pvalues_top_100.txt"), sep="\t",
+            row.names=F, col.names=T, quote=F)
