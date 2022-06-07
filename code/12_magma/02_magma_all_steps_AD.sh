ad8653452151babdec61d334328d8b53dfd29207
Sowmya Parthiban <sparthi1@jh.edu>
2022-06-07 11:07:01 -0400

    rename BFILE location
    rename gs_lc as gs_ad and change location

--- code/12_magma/02_magma_all_steps_AD.sh
+++ code/12_magma/02_magma_all_steps_AD.sh
@@ -19,12 +19,11 @@ module load magma/1.10
 ## Set some variables/paths
 model="snp-wise"
 ANNO=/dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/annotation/GRCh38_gencode.v32_Ensembl98_LIFTED-to-hg19_expressedGenes.gene.loc
-BFILE=/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/Layer_Guesses/MAGMA/g1000_eur
+BFILE=/dcs04/lieber/lcolladotor/with10x_LIBD001/HumanPilot/Analysis/Layer_Guesses/MAGMA/g1000_eur
 
 setcol=1
 genecol=2
-gs_lc=/dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/lcMarkerSets_fdr1e-6.txt
-
+gs_ad=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/pvalues_top_100.txt
 SUMMSTATS=/dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/GWAS_Results/AD_sumstats_Jansenetal_2019sept.txt
 
 
@@ -38,7 +37,7 @@ echo "MNT comment 29May2022: just run steps 1-2 to confirm concordance b/tw GWAS
 
 
 ## Step 3 - Gene set analyses (using gene-level output)
-magma --gene-results SNP_Data/AD_Jansen2019_LC_snp-wise.genes.raw --set-annot $gs_lc gene-col=${genecol} set-col=${setcol} --out Results/lc_Alzheimers
+magma --gene-results SNP_Data/AD_Jansen2019_LC_snp-wise.genes.raw --set-annot $gs_ad gene-col=${genecol} set-col=${setcol} --out Results/lc_Alzheimers
 
 echo "**** Job ends ****"
 date
