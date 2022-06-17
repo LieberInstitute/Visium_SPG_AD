b0c682951c8bd2e036350ac37253758940f38b37
Sowmya Parthiban <sparthi1@jh.edu>
2022-06-16 21:31:16 -0400

    remove targeted data
    update spe object to 08_harmony_BayesSpace
    
    update sample_id to sample_id_short
    
    update plot label to greek letter

--- code/09_pathology_vs_BayesSpace/01_bayesspace_k_pathology_boxplots.R
+++ code/09_pathology_vs_BayesSpace/01_bayesspace_k_pathology_boxplots.R
@@ -13,12 +13,10 @@ library("sessioninfo")
 ## Load basic SPE data
 spe_wholegenome <- readRDS(
     here::here(
-        "processed-data", "07_spot_qc", "spe_wholegenome_postqc.rds"
-    )
-)
-spe_targeted <- readRDS(
-    here::here(
-        "processed-data", "07_spot_qc", "spe_targeted_postqc.rds"
+        "processed-data",
+        "08_harmony_BayesSpace",
+        "wholegenome",
+        "spe_harmony_wholegenome.rds"
     )
 )
 
@@ -35,21 +33,12 @@ spe_wholegenome <- cluster_import(
     prefix = ""
 )
 
-# import cluster info for targeted genome
-dir_rdata_targeted <- here::here("processed-data", "08_harmony_BayesSpace", "targeted")
-
-spe_targeted <- cluster_import(
-    spe_targeted,
-    cluster_dir = file.path(dir_rdata_targeted, "clusters_BayesSpace"),
-    prefix = ""
-)
-
 
 # create df with relevant variables for whole
 cluster_whole_df <- data.frame(
     spot_id = colnames(spe_wholegenome),
     diagnosis = spe_wholegenome$diagnosis,
-    sample_id = factor(spe_wholegenome$sample_id, levels = unique(spe_wholegenome$sample_id)),
+    sample_id_short = factor(spe_wholegenome$sample_id_short, levels = unique(spe_wholegenome$sample_id_short)),
     NAbeta = spe_wholegenome$NAbeta,
     NpTau = spe_wholegenome$NpTau,
     PAbeta = spe_wholegenome$PAbeta,
@@ -58,77 +47,60 @@ cluster_whole_df <- data.frame(
     ## have to convert entire colData to df
 )
 
-# create df with relevant variables for targeted
-cluster_targeted_df <- data.frame(
-    spot_id = colnames(spe_targeted),
-    diagnosis = spe_targeted$diagnosis,
-    sample_id = factor(spe_targeted$sample_id, levels = unique(spe_targeted$sample_id)),
-    NAbeta = spe_targeted$NAbeta,
-    NpTau = spe_targeted$NpTau,
-    PAbeta = spe_targeted$PAbeta,
-    PpTau = spe_targeted$PpTau,
-    colData(spe_targeted)[grep("BayesSpace_harmony_k", colnames(colData(spe_targeted)))]
-)
-
 
 # convert cluster info to factor in cluster_whole_df
 cols_whole <- colnames(cluster_whole_df |> select(matches("harmony")))
 cluster_whole_df[cols_whole] <- lapply(cluster_whole_df[cols_whole], factor)
 
-# convert cluster info to factor in cluster_targeted_df
-cols_targeted <- colnames(cluster_targeted_df |> select(matches("harmony")))
-cluster_targeted_df[cols_targeted] <- lapply(cluster_targeted_df[cols_targeted], factor)
 
 
+# create plots
 
-pathology_measures <- c("PpTau", "PAbeta")
+measure = "PAbeta"
+pdf(file.path(dir_plots, paste0("proportion_of_", measure, ".pdf")), width = 18)
 
-# create plots for whole genome
-for (measure in pathology_measures) {
-    pdf(file.path(dir_plots, paste0("spe_wholegenome", "_", measure, ".pdf")), width = 18)
-
-    for (i in cols_whole) {
-        plot <- ggpubr::ggviolin(
-            cluster_whole_df,
-            i,
-            measure,
-            add = "boxplot",
-            add.params = list(fill = "white")
-        )
-        plot <- facet(plot + theme_bw(),
-            facet.by = "sample_id",
-            short.panel.labs = FALSE,
-            scales = "free_y"
-        )
-
-
-        print(plot)
-    }
-    dev.off()
+for (i in cols_whole) {
+    plot <- ggpubr::ggviolin(
+        cluster_whole_df,
+        i,
+        measure,
+        add = "boxplot",
+        add.params = list(fill = "white"),
+        ylab = expression(paste("Proportion of A", beta , " per spot "))
+    )
+    plot <- facet(plot + theme_bw(),
+                  facet.by = "sample_id_short",
+                  short.panel.labs = TRUE,
+                  scales = "free_y"
+    )
+    print(plot)
 }
+dev.off()
+
+measure = "PpTau"
+
+pdf(file.path(dir_plots, paste0("proportion_of_", measure, ".pdf")), width = 18)
+
+for (i in cols_whole) {
+    plot <- ggpubr::ggviolin(
+        cluster_whole_df,
+        i,
+        measure,
+        add = "boxplot",
+        add.params = list(fill = "white"),
+        ylab = expression(paste("Proportion of pTau per spot"))
+    )
+    plot <- facet(plot + theme_bw(),
+                  facet.by = "sample_id_short",
+                  short.panel.labs = TRUE,
+                  scales = "free_y"
+    )
 
 
-# create plots for targeted genome
-for (measure in pathology_measures) {
-    pdf(file.path(dir_plots, paste0("spe_targeted", "_", measure, ".pdf")), width = 18)
-    for (i in cols_targeted) {
-        plot <- ggpubr::ggviolin(
-            cluster_targeted_df,
-            i,
-            measure,
-            add = "boxplot",
-            add.params = list(fill = "white")
-        )
-        plot <- facet(plot + theme_bw(),
-            facet.by = "sample_id",
-            short.panel.labs = FALSE,
-            scales = "free_y"
-        )
-        print(plot)
-    }
-    dev.off()
+
+    print(plot)
 }
-
+dev.off()
 
 ## Reproducibility information
 print("Reproducibility information:")
