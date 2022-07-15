a7bff5fdb4d63fcca380db4646159b4e812ded9d
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-15 08:58:22 -0400

    notes for tge_vs_wg table sh script to run R code for tge_vs_wg table

---
+++ code/13_tge_panel/02_tge_vs_wg_table.R
@@ -0,0 +1,40 @@
+# library(sgejobs)
+# sgejobs::job_single(
+#         "02_tge_vs_wg_table",
+#         create_shell = TRUE,
+#         queue = "bluejay",
+#         memory = "10G",
+#         command = "Rscript 02_tge_vs_wg_table.R",
+#         create_logdir = TRUE
+#     )
+
+
+library(sessioninfo)
+library(here)
+library(dplyr)
+
+### Cloning to Github from RStudio https://www.youtube.com/watch?v=4s5yAxa99cE
+
+#### wg ====
+
+##TODO read file in
+#wg_input <-    read.csv(file = )
+#here package can be used to input file
+#input location Visium_IF_AD/code/05_deploy_app_wholegenome/Visium_IF_AD_wholegenome_model_results_FDR5perc.csv
+#input URL on Github #https://github.com/LieberInstitute/Visium_IF_AD/blob/master/code/05_deploy_app_wholegenome/Visium_IF_AD_wholegenome_model_results_FDR5perc.csv
+
+
+##Alternatively you can also input the file directly from github using
+#github link for input
+#library RCurl
+#x <- getURL
+#Y <- read.csv(text = x)
+#https://stackoverflow.com/questions/14441729/read-a-csv-from-github-into-r
+
+###
+
+
+#### tg ====
+
+
+
