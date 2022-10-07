1ddc6cb4b7914ceba77a39b14f63b4df1cc5ca42
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-10-07 18:41:07 -0400

    Update csv's with modeling results

--- code/05_deploy_app_wholegenome/app.R
+++ code/05_deploy_app_wholegenome/app.R
@@ -14,24 +14,19 @@ local <- FALSE ## For doing things locally and ability to change input dirs
 if (local) {
     ## Local tests
     dir_rdata <- here::here(
-        "processed-data",
-        "11_grey_matter_only",
-        "without_Br3873",
-        "wholegenome"
+        "code",
+        "05_deploy_app_wholegenome"
     )
 } else {
     ## For shinyapps.io
     dir_rdata <- getwd()
 }
-load(file.path(dir_rdata, "Visium_IF_AD_modeling_results.Rdata"), verbose = TRUE)
-sce_pseudo <- readRDS(file.path(dir_rdata, "sce_pseudo_pathology_wholegenome.rds"))
-
-## Fix column names
-colnames(modeling_results$pairwise) <- gsub(
-    "pos",
-    "+",
-    colnames(modeling_results$pairwise)
+load(file.path(dir_rdata, "Visium_IF_AD_modeling_results.Rdata"),
+    verbose = TRUE
 )
+sce_pseudo <-
+    readRDS(file.path(dir_rdata, "sce_pseudo_pathology_wholegenome.rds"))
+
 
 ## For sig_genes_extract_all() to work
 sce_pseudo$spatialLIBD <- sce_pseudo$path_groups
@@ -44,17 +39,24 @@ sig_genes <- sig_genes_extract_all(
 ## Extract FDR < 5%
 ## From
 ## https://github.com/LieberInstitute/brainseq_phase2/blob/be2b7f972bb2a0ede320633bf06abe1d4ef2c067/supp_tabs/create_supp_tables.R#L173-L181
-# fix_csv <- function(df) {
-#     for (i in seq_len(ncol(df))) {
-#         if (any(grepl(",", df[, i]))) {
-#             message(paste(Sys.time(), "fixing column", colnames(df)[i]))
-#             df[, i] <- gsub(",", ";", df[, i])
-#         }
-#     }
-#     return(df)
-# }
-# z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
-# write.csv(z, file = file.path(dir_rdata, "Visium_IF_AD_wholegenome_model_results_FDR5perc.csv"))
+if (local) {
+    fix_csv <- function(df) {
+        for (i in seq_len(ncol(df))) {
+            if (any(grepl(",", df[, i]))) {
+                message(paste(Sys.time(), "fixing column", colnames(df)[i]))
+                df[, i] <- gsub(",", ";", df[, i])
+            }
+        }
+        return(df)
+    }
+    z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
+    write.csv(z[, !grepl("^in_rows", colnames(z))],
+        file = file.path(
+            dir_rdata,
+            "Visium_IF_AD_wholegenome_model_results_FDR5perc.csv"
+        )
+    )
+}
 
 vars <- colnames(colData(spe))
 path_vars <- vars[grep("^path_", vars)]
