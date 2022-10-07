5d10e823fbf40640710b1e3b369af6831db8fc4d
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-10-07 19:04:43 -0400

    Add code for soft creating the required soft link

--- code/iSEE_app/deploy.R
+++ code/iSEE_app/deploy.R
@@ -4,6 +4,8 @@ library("rsconnect")
 source("token.R")
 
 options(repos = BiocManager::repositories())
+
+# system("ln -s ../05_deploy_app_wholegenome/sce_pseudo_pathology_wholegenome.rds sce_pseudo_pathology_wholegenome.rds")
 rsconnect::deployApp(
     appFiles = c("app.R", "sce_pseudo_pathology_wholegenome.rds", "initial.R"),
     appName = "Kwon2022_pseudobulk_AD_pathology_wholegenome",
