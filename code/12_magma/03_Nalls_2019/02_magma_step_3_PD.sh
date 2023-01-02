e87c14772f331e5ce2c131cf1e3109721c44a903
Sowmya Parthiban <sparthi1@jh.edu>
2023-01-02 16:17:07 +0530

    shorten paths

--- code/12_magma/03_Nalls_2019/02_magma_step_3_PD.sh
+++ code/12_magma/03_Nalls_2019/02_magma_step_3_PD.sh
@@ -24,10 +24,11 @@ model="snp-wise"
 # setcol=1
 # genecol=2
 
-gs_200=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/pvalues_top_200.txt
-gs_50=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/pvalues_top_50.txt
-gs_100=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/pvalues_top_100.txt
-fdr_set=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/fdr_gene_set.txt
+here=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma
+gs_200=$here/pvalues_top_200.txt
+gs_50=$here/pvalues_top_50.txt
+gs_100=$here/pvalues_top_100.txt
+fdr_set=$here/fdr_gene_set.txt
 #SUMMSTATS=/dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/GWAS_Results/nallsEtAl2019_excluding23andMe_allVariants_Neff-and-rsID-ADDED-MNT.tab
 
 
@@ -36,15 +37,16 @@ fdr_set=/dcs04/lieber/lcolladotor/with10x_LIBD001/Visium_IF_AD/code/12_magma/fdr
 ## Step 3 - Gene set analyses (using gene-level output)
 
 #top 200
-magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_200 gene-col=${genecol} set-col=${setcol} --out code/12_magma/03_Nalls_2019/pd_gwas_200
+magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_200 gene-col=${genecol} set-col=${setcol} --out $here/03_Nalls_2019/pd_gwas_200
 
 #top 100
-magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_100 gene-col=${genecol} set-col=${setcol} --out code/12_magma/03_Nalls_2019/pd_gwas_100
+magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_100 gene-col=${genecol} set-col=${setcol} --out $here/03_Nalls_2019/pd_gwas_100
 
 #top 50
-magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_50 gene-col=${genecol} set-col=${setcol} --out code/12_magma/03_Nalls_2019/pd_gwas_50
+magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $gs_50 gene-col=${genecol} set-col=${setcol} --out $here/03_Nalls_2019/pd_gwas_50
 
 #fdr geneset
-magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $fdr_set gene-col=${genecol} set-col=${setcol} --out code/12_magma/03_Nalls_2019/pd_gwas_fdr
+magma --gene-results /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/code/magma/SNP_Data/PD_Nalls2019_LC_snp-wise.genes.raw --set-annot $fdr_set gene-col=${genecol} set-col=${setcol} --out $here/03_Nalls_2019/pd_gwas_fdr
+
 
 
