d0b6b5f90058b2bd54c3aa01c85fed2c9c4f9444
Sowmya Parthiban <sparthib@compute-119.cm.cluster>
2023-01-02 23:49:41 -0500

    move heatmap code to new directory

---
+++ code/12_magma/heatmaps/magma_heatmap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -N magma_heatmap
+#$ -o logs/magma_heatmap.$TASK_ID.txt
+#$ -e logs/magma_heatmap.$TASK_ID.txt
+#$ -m e
+#$ -t 1-4
+#$ -tc 20
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript magma_heatmap.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
