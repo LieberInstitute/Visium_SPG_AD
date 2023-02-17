36e1eb09d84ae0faab500ff8d55ae366083d7acf
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-02-17 10:12:28 -0500

    Copied code from https://github.com/LieberInstitute/Visium_IF_AD/blob/d1d19b6c07b938823a027da75ebdf6858ff4a977/code/05_deploy_app_wholegenome/app.R#L24-L37 to start the 04_volcano.R script

---
+++ code/11_grey_matter_only/04_volcano.R
@@ -0,0 +1,14 @@
+load(file.path(dir_rdata, "Visium_IF_AD_modeling_results.Rdata"),
+    verbose = TRUE
+)
+sce_pseudo <-
+    readRDS(file.path(dir_rdata, "sce_pseudo_pathology_wholegenome.rds"))
+
+
+## For sig_genes_extract_all() to work
+sce_pseudo$spatialLIBD <- sce_pseudo$path_groups
+sig_genes <- sig_genes_extract_all(
+    n = nrow(sce_pseudo),
+    modeling_results = modeling_results,
+    sce_layer = sce_pseudo
+)
