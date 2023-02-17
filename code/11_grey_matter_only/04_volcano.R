c60552c8cd9cd43d005d10951f529b2abc85f8bd
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-02-17 10:30:11 -0500

    Started EnhancedVolcano code, but we need the logFC :/

--- code/11_grey_matter_only/04_volcano.R
+++ code/11_grey_matter_only/04_volcano.R
@@ -1,6 +1,7 @@
 library("here")
 library("sessioninfo")
 library("spatialLIBD")
+library("EnhancedVolcano")
 
 ## Locate data directory
 dir_rdata <- here::here(
@@ -22,6 +23,36 @@ sig_genes <- sig_genes_extract_all(
     modeling_results = modeling_results,
     sce_layer = sce_pseudo
 )
+
+## Build a data.frame with the info needed first
+## Adapted from https://github.com/LieberInstitute/spatial_hpc/blob/62334e88788c2a7010f2d500418639769121ecf1/code/08_pseudobulk/PRECAST/plot_volcano.R#L75-L80
+
+testname <- "Ab"
+make_volcano <- function(testname) {
+    subset_sig <- subset(sig_genes, test == testname & model_type == "enrichment")
+    df <- data.frame(
+        gene = subset_sig$gene,
+        stat = subset_sig$stat,
+        FDR = subset_sig$fdr,
+        sig = subset_sig$fdr < 0.05
+    )
+
+    ## Adapted from https://github.com/LieberInstitute/spatial_hpc/blob/62334e88788c2a7010f2d500418639769121ecf1/code/08_pseudobulk/PRECAST/plot_volcano.R#L580-L591
+    EnhancedVolcano(df,
+        lab = df$gene,
+        x = 'stat',
+        y = 'FDR',
+        FCcutoff = 2,
+        pCutoff = 0.05,
+        ylab = "-log10 FDR",
+        legendLabels = c('Not sig.','Log (base 2) FC','FDR',
+            'FDR & Log (base 2) FC'),
+        title = paste(testname, "vs others"),
+        subtitle = ""
+    )
+}
+
+
 
 ## Reproducibility information
 print("Reproducibility information:")
