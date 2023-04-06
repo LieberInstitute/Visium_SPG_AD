1fe98769c2f6152515d1d6f6f484b417999fe091
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-04-05 21:29:09 -0400

    Copy 05_deploy_app_wholegenome to create 18_deploy_app_wholegenome_Abeta_microenv

---
+++ code/18_deploy_app_wholegenome_Abeta_microenv/app.R
@@ -0,0 +1,105 @@
+library("spatialLIBD")
+library("markdown") ## Hm... to avoid this error
+# 2021-11-11T05:30:50.218127+00:00 shinyapps[5096402]: Warning: Error in loadNamespace: there is no package called ‘markdown’
+
+## spatialLIBD uses golem
+options("golem.app.prod" = TRUE)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Load the data
+load("spe.Rdata", verbose = TRUE)
+local <- FALSE ## For doing things locally and ability to change input dirs
+if (local) {
+    ## Local tests
+    dir_rdata <- here::here(
+        "code",
+        "05_deploy_app_wholegenome"
+    )
+} else {
+    ## For shinyapps.io
+    dir_rdata <- getwd()
+}
+load(file.path(dir_rdata, "Visium_SPG_AD_modeling_results.Rdata"),
+    verbose = TRUE
+)
+sce_pseudo <-
+    readRDS(file.path(dir_rdata, "sce_pseudo_pathology_wholegenome.rds"))
+
+## Change Braak info based on latest information from LIBD pathology
+sce_pseudo$BCrating <- NULL ## This variable was removed from the phenotype table
+sce_pseudo$braak <- c("Br3854" = "Stage VI", "Br3873" = "Stage V", "Br3880" = "Stage VI", "Br3874" = "Stage IV")[sce_pseudo$subject]
+sce_pseudo$cerad <- c("Br3854" = "Frequent", "Br3873" = "Frequent", "Br3880" = "Frequent", "Br3874" = "None")[sce_pseudo$subject]
+
+## For sig_genes_extract_all() to work
+sce_pseudo$spatialLIBD <- sce_pseudo$path_groups
+sig_genes <- sig_genes_extract_all(
+    n = nrow(sce_pseudo),
+    modeling_results = modeling_results,
+    sce_layer = sce_pseudo
+)
+
+## Extract FDR < 5%
+## From
+## https://github.com/LieberInstitute/brainseq_phase2/blob/be2b7f972bb2a0ede320633bf06abe1d4ef2c067/supp_tabs/create_supp_tables.R#L173-L181
+if (local) {
+    fix_csv <- function(df) {
+        for (i in seq_len(ncol(df))) {
+            if (any(grepl(",", df[, i]))) {
+                message(paste(Sys.time(), "fixing column", colnames(df)[i]))
+                df[, i] <- gsub(",", ";", df[, i])
+            }
+        }
+        return(df)
+    }
+    z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
+    write.csv(z[, !grepl("^in_rows", colnames(z))],
+        file = file.path(
+            dir_rdata,
+            "Visium_SPG_AD_wholegenome_model_results_FDR5perc.csv"
+        )
+    )
+}
+
+vars <- colnames(colData(spe))
+path_vars <- vars[grep("^path_", vars)]
+path_vars <- path_vars[!grepl("_colors$", path_vars)]
+
+## Deploy the website
+spatialLIBD::run_app(
+    spe,
+    sce_layer = sce_pseudo,
+    modeling_results = modeling_results,
+    sig_genes = sig_genes,
+    title = "Visium IF AD, Kwon SH et al, 2022",
+    spe_discrete_vars = c(
+        path_vars,
+        "ManualAnnotation",
+        vars[grep("^BayesSpace_", vars)],
+        vars[grep("^graph_", vars)],
+        "edge_spots",
+        vars[grep("^scran_", vars)],
+        vars[grep("^10x_", vars)]
+    ),
+    spe_continuous_vars = c(
+        "sum_umi",
+        "sum_gene",
+        "expr_chrM",
+        "expr_chrM_ratio",
+        "NAbeta",
+        "PAbeta",
+        "NDAPI",
+        "PDAPI",
+        "NGFAP",
+        "PGFAP",
+        "NLipofuscin",
+        "PLipofuscin",
+        "NMAP2",
+        "PMAP2",
+        "NpTau",
+        "PpTau",
+        "edge_distance"
+    ),
+    default_cluster = "path_groups"
+)
---
+++ code/18_deploy_app_wholegenome_Abeta_microenv/deploy.R
@@ -0,0 +1,28 @@
+library("rsconnect")
+library("here")
+
+## Or you can go to your shinyapps.io account and copy this
+## Here we do this to keep our information hidden.
+load(here("code", "05_deploy_app_wholegenome", ".deploy_info.Rdata"), verbose = TRUE)
+rsconnect::setAccountInfo(
+    name = deploy_info$name,
+    token = deploy_info$token,
+    secret = deploy_info$secret
+)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Deploy the app, that is, upload it to shinyapps.io
+rsconnect::deployApp(
+    appDir = here("code", "05_deploy_app_wholegenome"),
+    appFiles = c(
+        "app.R",
+        "spe.Rdata",
+        "Visium_SPG_AD_modeling_results.Rdata",
+        "sce_pseudo_pathology_wholegenome.rds"
+    ),
+    appName = "Visium_SPG_AD_Kwon2022",
+    account = "libd",
+    server = "shinyapps.io"
+)
