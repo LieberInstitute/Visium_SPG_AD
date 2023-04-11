40171a517a6e42935f9234258f2cdaf0bbc15b4f
Nick-Eagles <nick.eagles@libd.org>
2023-04-11 16:13:20 -0400

    Request more memory

--- code/16_samui/01-SPG_images.sh
+++ code/16_samui/01-SPG_images.sh
@@ -3,7 +3,7 @@
 #$ -N "SPG_images"
 #$ -o ../../processed-data/16_samui/logs/01-SPG_images_$TASK_ID.log
 #$ -e ../../processed-data/16_samui/logs/01-SPG_images_$TASK_ID.log
-#$ -l mf=10G,h_vmem=10G
+#$ -l mf=20G,h_vmem=20G
 #$ -t 1
 #$ -tc 1
 
@@ -16,7 +16,7 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 
-module load loopy/1.0.0-next.15
+module load loopy/1.0.0-next.24
 python 01-SPG_images.py
 
 echo "**** Job ends ****"
