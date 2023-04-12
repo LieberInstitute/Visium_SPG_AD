2eb1fdbb07f5cf27845f248b12b7cdb2ea03b939
Sowmya Parthiban <sparthi1@jh.edu>
2023-04-12 00:01:44 -0400

    add new arg

--- code/10_spatial_registration/spatial_registration_targeted.sh
+++ code/10_spatial_registration/spatial_registration_targeted.sh
@@ -1,1 +1,36 @@
-#!/bin/bash#$ -cwd#$ -l bluejay,mem_free=50G,h_vmem=50G,h_fsize=100G#$ -N spatial_registration_targeted#$ -o logs/spatial_registration_targeted.$TASK_ID.txt#$ -e logs/spatial_registration_targeted.$TASK_ID.txt#$ -m e#$ -t 2-28#$ -tc 20echo "**** Job starts ****"dateecho "**** JHPCE info ****"echo "User: ${USER}"echo "Job id: ${JOB_ID}"echo "Job name: ${JOB_NAME}"echo "Hostname: ${HOSTNAME}"echo "Task id: ${SGE_TASK_ID}"## Load the R module (absent since the JHPCE upgrade to CentOS v7)module load conda_R/4.2## List current modules for reproducibilitymodule list## Edit with your job commandRscript 01_spatial_registration.R "targeted"echo "**** Job ends ****"date## This script was made using sgejobs version 0.99.1## available from http://research.libd.org/sgejobs/
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -N spatial_registration_targeted
+#$ -o logs/spatial_registration_targeted.$TASK_ID.txt
+#$ -e logs/spatial_registration_targeted.$TASK_ID.txt
+#$ -m e
+#$ -t 2-28
+#$ -tc 20
+#$ -hold_jid BayesSpace_k_search_targeted
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 01_spatial_registration.R "targeted"
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
