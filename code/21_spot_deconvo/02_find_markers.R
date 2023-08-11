3b8c799b522013282d5faad9c1d05dba19c538db
Nick-Eagles <nick.eagles@libd.org>
2023-08-11 16:55:47 -0400

    Skip the marker-finding piece of the script and run the rest

--- code/21_spot_deconvo/02_find_markers.R
+++ code/21_spot_deconvo/02_find_markers.R
@@ -137,21 +137,22 @@ dir.create(plot_dir, showWarnings = FALSE)
 
 sce = readRDS(sce_in)
 
-message("Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...")
-marker_stats = get_mean_ratio2(
-    sce, cellType_col = cell_type_var, assay_name = "counts"
-)
-marker_stats_1vall <- findMarkers_1vAll(
-    sce, cellType_col = cell_type_var, assay_name = "counts",
-    mod = find_markers_model
-)
-marker_stats <- left_join(
-    marker_stats, marker_stats_1vall,
-    by = c("gene", "cellType.target")
-)
+# message("Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...")
+# marker_stats = get_mean_ratio2(
+#     sce, cellType_col = cell_type_var, assay_name = "counts"
+# )
+# marker_stats_1vall <- findMarkers_1vAll(
+#     sce, cellType_col = cell_type_var, assay_name = "counts",
+#     mod = find_markers_model
+# )
+# marker_stats <- left_join(
+#     marker_stats, marker_stats_1vall,
+#     by = c("gene", "cellType.target")
+# )
 
-#   Save 'marker_stats' table and the markers themselves (just gene symbols)
-saveRDS(marker_stats, file.path(out_dir, 'marker_stats.rds'))
+# #   Save 'marker_stats' table and the markers themselves (just gene symbols)
+# saveRDS(marker_stats, file.path(out_dir, 'marker_stats.rds'))
+marker_stats = readRDS(file.path(out_dir, 'marker_stats.rds'))
 write_markers(marker_stats, n_markers_per_type, file.path(out_dir, 'markers.txt'))
 
 ################################################################################
