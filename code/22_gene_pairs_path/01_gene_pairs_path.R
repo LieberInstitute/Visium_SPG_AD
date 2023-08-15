8a17c1f665d622a232c0d4464b5e170008ebbf8d
lcolladotor <lcolladotor@gmail.com>
2023-08-15 12:57:40 -0400

    Removed simple test for 10 genes that I coded in two different ways to verify that I was getting the results I was looking to.
    The loop version is much slower and thus was only useful to verify that the results were the intended ones wiht this small set of 10 genes.

--- code/22_gene_pairs_path/01_gene_pairs_path.R
+++ code/22_gene_pairs_path/01_gene_pairs_path.R
@@ -6,21 +6,7 @@ sce_pseudo <- spatialLIBD::fetch_data("Visium_SPG_AD_Visium_wholegenome_pseudobu
 spe_expr <- spe[rownames(sce_pseudo), spe$diagnosis == "AD"]
 assay(spe_expr, "expr") <- counts(spe_expr) > 0
 
-gene_combn <- combn(10, 2)
-ncol(gene_combn)
 
-Sys.time()
-co_expr_loop <- do.call(rbind, apply(gene_combn, 2, function(x) {
-    pair <- assay(spe_expr, "expr")[x, ]
-    pair[1, , drop = FALSE] & pair[2, , drop = FALSE]
-}))
-Sys.time()
-
-
-pair_1 <- assay(spe_expr, "expr")[gene_combn[1, ], ]
-pair_2 <- assay(spe_expr, "expr")[gene_combn[2, ], ]
-co_expr_v2 <- pair_1 & pair_2
-identical(co_expr_loop, co_expr_v2)
 
 
 
