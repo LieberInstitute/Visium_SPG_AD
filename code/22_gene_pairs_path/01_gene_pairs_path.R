8c799e8380f21f46234794642970e9e919d196ec
lcolladotor <lcolladotor@gmail.com>
2023-08-15 12:55:24 -0400

    Start a script for exploring the feasibility of the idea discussed at https://jhu-genomics.slack.com/files/U02GUMX92/F05N8N7L30R/img_2774.jpg with Sang Ho @shkwon17 and Nick Eagles @Nick-Eagles

---
+++ code/22_gene_pairs_path/01_gene_pairs_path.R
@@ -0,0 +1,77 @@
+library("spatialLIBD")
+spe <- spatialLIBD::fetch_data(type = "Visium_SPG_AD_Visium_wholegenome_spe")
+sce_pseudo <- spatialLIBD::fetch_data("Visium_SPG_AD_Visium_wholegenome_pseudobulk_spe")
+
+## Subset to genes observed
+spe_expr <- spe[rownames(sce_pseudo), spe$diagnosis == "AD"]
+assay(spe_expr, "expr") <- counts(spe_expr) > 0
+
+gene_combn <- combn(10, 2)
+ncol(gene_combn)
+
+Sys.time()
+co_expr_loop <- do.call(rbind, apply(gene_combn, 2, function(x) {
+    pair <- assay(spe_expr, "expr")[x, ]
+    pair[1, , drop = FALSE] & pair[2, , drop = FALSE]
+}))
+Sys.time()
+
+
+pair_1 <- assay(spe_expr, "expr")[gene_combn[1, ], ]
+pair_2 <- assay(spe_expr, "expr")[gene_combn[2, ], ]
+co_expr_v2 <- pair_1 & pair_2
+identical(co_expr_loop, co_expr_v2)
+
+
+gene_combn <- combn(1000, 2)
+ncol(gene_combn)
+# [1] [1] 499500
+
+Sys.time()
+# [1] "2023-08-15 12:25:31 EDT"
+pair_1 <- assay(spe_expr, "expr")[gene_combn[1, ], ]
+Sys.time()
+# [1] "2023-08-15 12:25:37 EDT"
+lobstr::obj_size(pair_1)
+# 11.35 GB
+Sys.time()
+pair_2 <- assay(spe_expr, "expr")[gene_combn[2, ], ]
+Sys.time()
+# [1] "2023-08-15 12:26:30 EDT"
+lobstr::obj_size(pair_2)
+# 11.34 GB
+Sys.time()
+co_expr <- pair_1 & pair_2
+# Error: vector memory exhausted (limit reached?)
+Sys.time()
+
+
+
+
+
+gene_combn <- combn(500, 2)
+ncol(gene_combn)
+# [1] 124750
+
+Sys.time()
+# [1] "2023-08-15 12:32:14 EDT"
+pair_1 <- assay(spe_expr, "expr")[gene_combn[1, ], ]
+Sys.time()
+# [1] "2023-08-15 12:32:15 EDT"
+lobstr::obj_size(pair_1)
+# 2.91 GB
+Sys.time()
+# [1] "2023-08-15 12:32:15 EDT"
+pair_2 <- assay(spe_expr, "expr")[gene_combn[2, ], ]
+Sys.time()
+# [1] "2023-08-15 12:32:22 EDT"
+lobstr::obj_size(pair_2)
+# 2.71 GB
+Sys.time()
+# [1] "2023-08-15 12:32:22 EDT"
+co_expr <- pair_1 & pair_2
+# Error: vector memory exhausted (limit reached?)
+Sys.time()
+# [1] "2023-08-15 12:32:56 EDT"
+lobstr::obj_size(co_expr)
+# 389.46 MB
