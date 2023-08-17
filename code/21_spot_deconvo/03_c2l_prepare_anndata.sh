99c545b0d44d9fc40ac6d82fb8bb3ba9f168d175
Nick-Eagles <nick.eagles@libd.org>
2023-08-17 14:21:44 -0400

    Prepare anndatas with the correct SpatialExperiment. Part of #82

--- code/21_spot_deconvo/03_c2l_prepare_anndata.sh
+++ code/21_spot_deconvo/03_c2l_prepare_anndata.sh
@@ -4,7 +4,7 @@
 #$ -N "c2l_prepare_anndata"
 #$ -o ../../processed-data/21_spot_deconvo/logs/03_c2l_prepare_anndata.log
 #$ -e ../../processed-data/21_spot_deconvo/logs/03_c2l_prepare_anndata.log
-#$ -l mf=40G,h_vmem=40G,h_fsize=50G
+#$ -l caracol,mf=40G,h_vmem=40G,h_fsize=50G
 
 #SBATCH -q shared
 #SBATCH --mem=40G
@@ -12,14 +12,14 @@
 #SBATCH -o ../../processed-data/21_spot_deconvo/logs/03_c2l_prepare_anndata.log
 #SBATCH -e ../../processed-data/21_spot_deconvo/logs/03_c2l_prepare_anndata.log
 
-USE_SLURM=0
-
-if [[ $USE_SLURM -eq 1 ]]; then
+if [[ ! -z $SLURMD_NODENAME ]]; then
     job_id=$SLURM_JOB_ID
     job_name=$SLURM_JOB_NAME
+    node_name=$SLURMD_NODENAME
 else
     job_id=$JOB_ID
     job_name=$JOB_NAME
+    node_name=$HOSTNAME
 fi
 
 echo "**** Job starts ****"
@@ -28,9 +28,9 @@ echo "**** JHPCE info ****"
 echo "User: ${USER}"
 echo "Job id: ${job_id}"
 echo "Job name: ${job_name}"
-echo "Hostname: ${HOSTNAME}"
+echo "Node name: ${node_name}"
 
-module load cell2location/0.8a0
+module load cell2location/0.1.3
 python 03_c2l_prepare_anndata.py
 
 echo "**** Job ends ****"
