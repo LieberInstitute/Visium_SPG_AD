2c476732c7dfedcd1bbbf8e4db5906d631a9deb8
lcolladotor <lcolladotor@gmail.com>
2023-08-22 10:21:20 -0400

    Made a new plot to replace Figure S9 panel A

--- code/07_spot_qc/05_qc_plots_revision.R
+++ code/07_spot_qc/05_qc_plots_revision.R
@@ -62,6 +62,30 @@ palette_colors <- paste0(
 )
 names(palette_colors) <- levels(spe_wholegenome$sample_id_short)
 
+
+## Re-order samples to match order in other figures
+sample_order <- c(
+    "V10A27106_B1_Br3854",
+    "V10A27106_C1_Br3873",
+    "V10A27106_D1_Br3880",
+    "V10T31036_B1_Br3854",
+    "V10T31036_C1_Br3873",
+    "V10T31036_D1_Br3880",
+    "V10A27004_D1_Br3880",
+    "V10A27106_A1_Br3874",
+    "V10T31036_A1_Br3874",
+    "V10A27004_A1_Br3874"
+)
+stopifnot(length(unique(sample_order)) == 10)
+new_order <- unlist(lapply(sample_order, function(i) {
+    which(spe_wholegenome$sample_id == i)
+}))
+stopifnot(all(seq_len(ncol(spe_wholegenome)) %in% new_order))
+spe_wholegenome <- spe_wholegenome[, new_order]
+spe_targeted <- spe_targeted[, new_order]
+
+
+
 ## Metrics QC
 metrics_qc <- function(spe, spename) {
     qcstats <- perCellQCMetrics(spe, subsets = list(
@@ -75,21 +99,6 @@ metrics_qc <- function(spe, spename) {
         sample_id = spe$sample_id_short
     )
 
-    for (i in c("log2sum", "log2detected", "subsets_Mito_percent")) {
-        pdf(file.path(dir_plots, paste0("scran_", spename, "_distribution_", i, ".pdf")), width = 14)
-        p <- ggpubr::ggviolin(
-            qc_df,
-            "sample_id",
-            i,
-            fill = "sample_id",
-            palette = palette_colors,
-            add = "boxplot",
-            add.params = list(fill = "white")
-        )
-        print(p)
-        dev.off()
-    }
-
     qcfilter <- DataFrame(
         low_lib_size = isOutlier(qcstats$sum, type = "lower", log = TRUE, batch = spe$sample_id_short),
         low_n_features = isOutlier(qcstats$detected, type = "lower", log = TRUE, batch = spe$sample_id_short),
@@ -98,36 +107,7 @@ metrics_qc <- function(spe, spename) {
     qcfilter$discard <- (qcfilter$low_lib_size | qcfilter$low_n_features) | qcfilter$high_subsets_Mito_percent
 
 
-    pdf(file.path(
-        dir_plots,
-        paste0("scran_", spename, "_low_lib_size_vs_mito_scatter.pdf")
-    ), width = 21)
-    p <- ggplot(
-        qc_df,
-        aes(
-            x = log2sum,
-            y = subsets_Mito_percent,
-            color = qcfilter$low_lib_size,
-            shape = isOutlier(qcstats$subsets_Mito_percent, type = "lower", batch = spe$sample_id_short)
-        )
-    ) +
-        geom_point() +
-        facet_grid(~sample_id) +
-        guides(color = guide_legend("Low lib?")) +
-        guides(shape = guide_legend("Low mito?"))
-    print(p)
-    dev.off()
-
     spe$scran_low_lib_size_low_mito <- factor(qcfilter$low_lib_size & qc_df$subsets_Mito_percent < 0.5, levels = c("TRUE", "FALSE"))
-    vis_grid_clus(
-        spe = spe,
-        clustervar = "scran_low_lib_size_low_mito",
-        pdf = file.path(dir_plots, paste0("scran_", spename, "_low_lib_size_vs_mito.pdf")),
-        sort_clust = FALSE,
-        colors = c("FALSE" = "grey90", "TRUE" = "orange"),
-        spatial = FALSE,
-        point_size = 2
-    )
 
 
     spe$scran_discard <-
@@ -138,19 +118,6 @@ metrics_qc <- function(spe, spename) {
         factor(qcfilter$low_n_features, levels = c("TRUE", "FALSE"))
     spe$scran_high_subsets_Mito_percent <-
         factor(qcfilter$high_subsets_Mito_percent, levels = c("TRUE", "FALSE"))
-
-    for (i in colnames(qcfilter)) {
-        vis_grid_clus(
-            spe = spe,
-            clustervar = paste0("scran_", i),
-            pdf = file.path(dir_plots, paste0("scran_", spename, "_", i, ".pdf")),
-            sort_clust = FALSE,
-            colors = c("FALSE" = "grey90", "TRUE" = "orange"),
-            spatial = FALSE,
-            point_size = 2
-        )
-    }
-
 
     ## Find edge spots
     spots <- data.frame(
@@ -177,53 +144,9 @@ metrics_qc <- function(spe, spename) {
 
     spe$edge_spots <- factor(spots$edge_spots, levels = c("TRUE", "FALSE"))
     spe$edge_distance <- spots$edge_distance
-    vis_grid_clus(
-        spe = spe,
-        clustervar = "edge_spots",
-        pdf = file.path(dir_plots, paste0("egde_spots_", spename, ".pdf")),
-        sort_clust = FALSE,
-        colors = c("FALSE" = "grey90", "TRUE" = "orange"),
-        spatial = FALSE,
-        point_size = 2
-    )
 
-    vis_grid_gene(
-        spe = spe,
-        geneid = "edge_distance",
-        pdf = file.path(dir_plots, paste0("egde_distance_", spename, ".pdf")),
-        spatial = FALSE,
-        point_size = 2
-    )
-
-
-    pdf(file.path(
-        dir_plots,
-        paste0("scran_", spename, "_low_lib_size_vs_edge_distance_scatter.pdf")
-    ), width = 21)
-    p <- ggplot(
-        qc_df,
-        aes(
-            x = log2sum,
-            y = spots$edge_distance,
-            color = qcfilter$low_lib_size
-        )
-    ) +
-        geom_point() +
-        facet_grid(~sample_id) +
-        guides(color = guide_legend("Low lib?"))
-    print(p)
-    dev.off()
 
     spe$scran_low_lib_size_edge <- factor(qcfilter$low_lib_size & spots$edge_distance < 1, levels = c("TRUE", "FALSE"))
-    vis_grid_clus(
-        spe = spe,
-        clustervar = "scran_low_lib_size_edge",
-        pdf = file.path(dir_plots, paste0("scran_", spename, "_low_lib_size_vs_edge_distance.pdf")),
-        sort_clust = FALSE,
-        colors = c("FALSE" = "grey90", "TRUE" = "orange"),
-        spatial = FALSE,
-        point_size = 2
-    )
 
     return(spe)
 }
@@ -232,79 +155,6 @@ metrics_qc <- function(spe, spename) {
 spe_wholegenome <- metrics_qc(spe_wholegenome, "wholegenome")
 spe_targeted <- metrics_qc(spe_targeted, "targeted")
 
-
-## Segmentation spot QC
-seg_df <- data.frame(
-    Percent_Abeta = spe_wholegenome$PAbeta,
-    Percent_DAPI = spe_wholegenome$PDAPI,
-    Percent_pTau = spe_wholegenome$PpTau,
-    Number_Abeta = spe_wholegenome$NAbeta,
-    Number_DAPI = spe_wholegenome$NDAPI,
-    Number_pTau = spe_wholegenome$NpTau,
-    sample_id = spe_wholegenome$sample_id_short,
-    check.names = FALSE
-)
-
-for (i in paste0(rep(c("Percent_", "Number_"), each = 3), rep(c("Abeta", "DAPI", "pTau"), 2))) {
-    pdf(file.path(dir_plots, paste0("segmentation_distribution_", i, ".pdf")), width = 14)
-    p <- ggpubr::ggviolin(
-        seg_df,
-        "sample_id",
-        i,
-        fill = "sample_id",
-        palette = palette_colors,
-        add = "boxplot",
-        add.params = list(fill = "white")
-    )
-    print(p)
-    dev.off()
-}
-
-segqcfilter <- DataFrame(
-    Number_DAPI = isOutlier(spe_wholegenome$NDAPI, type = "higher", batch = spe_wholegenome$sample_id_short, nmads = 3),
-    Percent_pTau = isOutlier(spe_wholegenome$PpTau, type = "higher", batch = spe_wholegenome$sample_id_short, nmads = 5)
-)
-segqcfilter$discard_segmentation <- segqcfilter$Number_DAPI | segqcfilter$Percent_pTau
-
-attr(segqcfilter$Number_DAPI, "thresholds")
-#        S1_A1_Br3874 S1_B1_Br3854 S1_C1_Br3873 S1_D1_Br3880 S2_A1_Br3874
-# lower          -Inf         -Inf         -Inf         -Inf         -Inf
-# higher      11.8956      12.8956      12.8956      13.8956      12.8956
-#        S2_B1_Br3854 S2_C1_Br3873 S2_D1_Br3880 S3_A1_Br3874 S3_D1_Br3880
-# lower          -Inf         -Inf         -Inf         -Inf         -Inf
-# higher      12.8956      12.8956      13.8956       7.4478      14.8956
-attr(segqcfilter$Percent_pTau, "thresholds")
-#        S1_A1_Br3874 S1_B1_Br3854 S1_C1_Br3873 S1_D1_Br3880 S2_A1_Br3874
-# lower          -Inf         -Inf         -Inf         -Inf         -Inf
-# higher            0   0.01437132            0   0.02798106    0.0012306
-#        S2_B1_Br3854 S2_C1_Br3873 S2_D1_Br3880 S3_A1_Br3874 S3_D1_Br3880
-# lower          -Inf         -Inf         -Inf         -Inf         -Inf
-# higher   0.03801507            0   0.04665947            0   0.03044226
-
-spe_wholegenome$scran_discard_segmentation <-
-    factor(segqcfilter$discard_segmentation, levels = c("TRUE", "FALSE"))
-spe_wholegenome$scran_Number_DAPI <-
-    factor(segqcfilter$Number_DAPI, levels = c("TRUE", "FALSE"))
-spe_wholegenome$scran_Percent_pTau <-
-    factor(segqcfilter$Percent_pTau, levels = c("TRUE", "FALSE"))
-
-for (i in colnames(segqcfilter)) {
-    vis_grid_clus(
-        spe = spe_wholegenome,
-        clustervar = paste0("scran_", i),
-        pdf = file.path(dir_plots, paste0("scran_", i, ".pdf")),
-        sort_clust = FALSE,
-        colors = c("FALSE" = "grey90", "TRUE" = "orange"),
-        spatial = FALSE,
-        point_size = 2
-    )
-}
-
-## Don't drop any spots due to isOutlier() results on the image segmentation
-## Drop this information since we won't use for downstream analyses.
-spe_wholegenome$scran_discard_segmentation <- NULL
-spe_wholegenome$scran_Number_DAPI <- NULL
-spe_wholegenome$scran_Percent_pTau <- NULL
 
 ## Read information about the glare spots manually identified by Madhavi Tippani
 ## and Sang Ho Kwon.
@@ -319,15 +169,17 @@ m <- sapply(
 glare$sample_id <- spe_wholegenome$sample_id[m]
 glare$key <- with(glare, paste0(spotID, "_", sample_id))
 
-## Locate and drop the glare spots
+## Locate the glare spots
 m <- match(glare$key, spe_wholegenome$key)
 stopifnot(all(!is.na(m)))
 stopifnot(identical(m, match(glare$key, spe_targeted$key)))
-spe_wholegenome <- spe_wholegenome[, -m]
-spe_targeted <- spe_targeted[, -m]
+# spe_wholegenome <- spe_wholegenome[, -m]
+# spe_targeted <- spe_targeted[, -m]
+spe_targeted$glare <- spe_wholegenome$glare <- FALSE
+spe_targeted$glare[m] <- spe_wholegenome$glare[m] <- TRUE
 
 
-## Drop low library size spots on the edge for either whole genome or
+## Locate low library size spots on the edge for either whole genome or
 ## targeted sequencing
 addmargins(table("wholegenome" = spe_wholegenome$scran_low_lib_size_edge, "targeted" = spe_targeted$scran_low_lib_size_edge))
 #            targeted
@@ -337,15 +189,56 @@ addmargins(table("wholegenome" = spe_wholegenome$scran_low_lib_size_edge, "targe
 #       Sum     133 38134 38267
 
 drop_low_library_edge_either <- spe_wholegenome$scran_low_lib_size_edge == "TRUE" | spe_targeted$scran_low_lib_size_edge == "TRUE"
-spe_wholegenome <- spe_wholegenome[, !drop_low_library_edge_either]
-spe_targeted <- spe_targeted[, !drop_low_library_edge_either]
+# spe_wholegenome <- spe_wholegenome[, !drop_low_library_edge_either]
+# spe_targeted <- spe_targeted[, !drop_low_library_edge_either]
 
 ## Clean up some variables names
-spe_targeted$scran_low_lib_size_edge <- spe_wholegenome$scran_low_lib_size_edge <- NULL
+# spe_targeted$scran_low_lib_size_edge <- spe_wholegenome$scran_low_lib_size_edge <- NULL
 
-## Save for later
-saveRDS(spe_wholegenome, file = file.path(dir_rdata, "spe_wholegenome_postqc.rds"))
-saveRDS(spe_targeted, file = file.path(dir_rdata, "spe_targeted_postqc.rds"))
+spe_wholegenome$drop_low_library_edge_either <- spe_targeted$drop_low_library_edge_either <- drop_low_library_edge_either
+
+table(spe_wholegenome$drop_low_library_edge_either, spe_wholegenome$glare)
+#       FALSE  TRUE
+# FALSE 38115    20
+# TRUE    152     0
+
+table("lq edge" = spe_wholegenome$drop_low_library_edge_either, "glare" = spe_wholegenome$glare, "discard" = spe_wholegenome$scran_discard)
+# , , discard = TRUE
+#
+#        glare
+# lq edge FALSE  TRUE
+#   FALSE   930     0
+#   TRUE    148     0
+#
+# , , discard = FALSE
+#
+#        glare
+# lq edge FALSE  TRUE
+#   FALSE 37185    20
+#   TRUE      4     0
+
+
+spe_wholegenome$quality_groups <- "Pass"
+spe_wholegenome$quality_groups[spe_wholegenome$scran_discard == "TRUE"] <- "LQ: retained"
+spe_wholegenome$quality_groups[spe_wholegenome$glare] <- "LQ: glare"
+spe_wholegenome$quality_groups[spe_wholegenome$drop_low_library_edge_either] <- "LQ: low lib size & edge"
+table(spe_wholegenome$quality_groups)
+# LQ: glare LQ: low lib size & edge            LQ: retained                    Pass
+#        20                     152                     930                   37185
+
+p_list <- vis_grid_clus(
+    spe = spe_wholegenome,
+    clustervar = "quality_groups",
+    sort_clust = FALSE,
+    colors = c("Pass" = "grey90", "LQ: retained" = "orange", "LQ: glare" = "steelblue3", "LQ: low lib size & edge" = "violetred"),
+    spatial = FALSE,
+    point_size = 2,
+    return_plots = TRUE
+)
+
+pdf(file.path(dir_plots, "wholegenome_quality_groups.pdf"), useDingbats = FALSE, height = 8 * 4, width = 9 * 3)
+print(cowplot::plot_grid(plotlist = p_list, ncol = 3, align = "hv"))
+dev.off()
 
 ## Reproducibility information
 print("Reproducibility information:")
