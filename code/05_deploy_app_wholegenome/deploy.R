584622c0c9a46edda2463c881aa88c0acebc4fda
lcolladotor <lcolladotor@gmail.com>
2024-04-19 09:47:39 -0400

    No longer use the hidden token info, though leave the code commented in case it's needed later

--- code/05_deploy_app_wholegenome/deploy.R
+++ code/05_deploy_app_wholegenome/deploy.R
@@ -3,12 +3,12 @@ library("here")
 
 ## Or you can go to your shinyapps.io account and copy this
 ## Here we do this to keep our information hidden.
-load(here("code", "05_deploy_app_wholegenome", ".deploy_info.Rdata"), verbose = TRUE)
-rsconnect::setAccountInfo(
-    name = deploy_info$name,
-    token = deploy_info$token,
-    secret = deploy_info$secret
-)
+# load(here("code", "05_deploy_app_wholegenome", ".deploy_info.Rdata"), verbose = TRUE)
+# rsconnect::setAccountInfo(
+#     name = deploy_info$name,
+#     token = deploy_info$token,
+#     secret = deploy_info$secret
+# )
 
 ## You need this to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
